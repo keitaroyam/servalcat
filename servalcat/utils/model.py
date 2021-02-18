keitaroyam/servalcat

commit c4ae7633478ea229aaa885199ee0f98c5c951fec

API change in gemmi (sum_symmetry_equivalent_grid_points to symmetrize_sum) https://github.com/project-gemmi/gemmi/commit/aa51a66a9d32b96cbf6ef7b77a92facd48968ed8

servalcat/utils/model.py
@@ -99,7 +99,7 @@ def calc_fc_em(st, d_min, mott_bethe=True, monlib=None, blur=None, r_cut=1e-5, r
                 if cra.atom.is_hydrogen():
                     dc.add_c_contribution_to_grid(cra.atom, -1)
                     
-            dc.sum_symmetry_equivalent_grid_points()
+            dc.symmetrize_sum()
 
         grid = gemmi.transform_map_to_f_phi(dc.grid)
         asu_data = grid.prepare_asu_data(dmin=d_min, mott_bethe=True, unblur=dc.blur)

commit acfb2d669b5573859bcff086ebf30ce2141592f6

Fixed a mistake introduced in c4ae7633

servalcat/utils/model.py
@@ -106,7 +106,7 @@ def calc_fc_fft(st, d_min, source, mott_bethe=True, monlib=None, blur=None, r_cu
                 if cra.atom.is_hydrogen():
                     dc.add_c_contribution_to_grid(cra.atom, -1)
                     
-            dc.symmetrize_sum()
+            dc.grid.symmetrize_sum()
     else:
         dc.put_model_density_on_grid(st[0])
 