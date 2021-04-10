keitaroyam/servalcat

commit 6daf018cf47b61c44c4793320b3af591288761c5

run_refmac: stop generating shifted-back refined.mtz

servalcat/spa/shiftback.py
@@ -27,7 +27,6 @@ def add_arguments(parser):
     parser.add_argument('--ncsc_file',
                         help='')
     parser.add_argument('--output_prefix',
-                        default="refined",
                         help='output file prefix')
 # add_arguments()
 
@@ -71,7 +70,7 @@ def refmac_mtz_in_original_cell(org_cell, org_grid_size, new_grid_size, shifts, 
     mtz.write_to_file(mtz_out)
 # refmac_mtz_in_original_cell()
 
-def shift_back(xyz_in, refine_mtz, shifts_json, ncsc_in, out_prefix):
+def shift_back(xyz_in, shifts_json, ncsc_in=None, refine_mtz=None, out_prefix=None):
     logger.write("Reading shifts info from {}".format(shifts_json))
     info = json.load(open(shifts_json))
     for k in info:
@@ -82,12 +81,17 @@ def shift_back(xyz_in, refine_mtz, shifts_json, ncsc_in, out_prefix):
 
     if refine_mtz:
         logger.write("Transforming MTZ: {}".format(refine_mtz))
+        if out_prefix:
+            mtz_out = out_prefix+".mtz"
+        else:
+            mtz_out = utils.fileio.splitext(os.path.basename(refine_mtz))[0] + "_shiftback.mtz"
+            
         refmac_mtz_in_original_cell(org_cell,
                                     info["grid"],
                                     info["new_grid"],
                                     info["shifts"],
                                     refine_mtz,
-                                    out_prefix+".mtz")
+                                    mtz_out)
 
     if xyz_in:
         logger.write("Shifting back model: {}".format(xyz_in))
@@ -104,7 +108,8 @@ def shift_back(xyz_in, refine_mtz, shifts_json, ncsc_in, out_prefix):
             st.ncs.clear()
             st.ncs.extend([x for x in ncsops if not x.tr.is_identity()])
 
-        utils.fileio.write_model(st, out_prefix,
+        prefix = out_prefix if out_prefix else utils.fileio.splitext(os.path.basename(xyz_in))[0] + "_shiftback"
+        utils.fileio.write_model(st, prefix,
                                  pdb=True, cif=True, cif_ref=cif_ref)
         
 

servalcat/spa/run_refmac.py
@@ -218,7 +218,6 @@ def main(args):
     if not args.no_shift:
         ncsc_in = ("ncsc_global.txt") if has_ncsc else None
         spa.shiftback.shift_back(xyz_in=refmac_prefix+model_format,
-                                 refine_mtz=refmac_prefix+".mtz",
                                  shifts_json="shifts.json",
                                  ncsc_in=ncsc_in,
                                  out_prefix=args.output_prefix)
@@ -269,7 +268,6 @@ def main(args):
         if not args.no_shift:
             ncsc_in = ("ncsc_global.txt") if has_ncsc else None
             spa.shiftback.shift_back(xyz_in=refmac_prefix_shaken+model_format,
-                                     refine_mtz=refmac_prefix_shaken+".mtz",
                                      shifts_json="shifts.json",
                                      ncsc_in=ncsc_in,
                                      out_prefix=args.output_prefix+"_shaken_refined")

commit 7dbe605e225f6a971f579d5f538e5b8c480d5b3d

fix a bug introduced in 6daf018c

servalcat/spa/shiftback.py
@@ -121,8 +121,7 @@ def main(args):
         logger.write("ERROR: give --model and/or --refine_mtz")
         return
     
-    shift_back(args.model, args.refine_mtz, args.shifts,
-               args.ncsc_file, args.output_prefix)
+    shift_back(args.model, args.shifts, args.ncsc_file, args.refine_mtz, args.output_prefix)
 
 if __name__ == "__main__":
     import sys