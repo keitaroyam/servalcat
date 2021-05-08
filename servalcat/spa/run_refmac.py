keitaroyam/servalcat

commit 99654bba11e61b1548f4f95401644c54a301d959

support microheterogeneity but may become inconsistent with external restraints

servalcat/spa/run_refmac.py
@@ -157,6 +157,15 @@ $$""")
         logger.write(" FSCaverage(half2)= {: .4f}".format(sum(ncoeffs*fscvals[2])/sum_n))
 # calc_fsc()
 
+def modify_output(filename, inscode_mods=None):
+    if inscode_mods is None: return
+    
+    st, cif_ref = utils.fileio.read_structure_from_pdb_and_mmcif(filename)
+    utils.model.modify_inscodes_back(st, inscode_mods)
+    utils.fileio.write_model(st, prefix=utils.fileio.splitext(filename)[0],
+                             pdb=True, cif=True, cif_ref=cif_ref)
+# modify_output()
+
 def main(args):
     if not args.model:
         logger.write("Error: give --model.")
@@ -244,9 +253,11 @@ def main(args):
                                  ncsc_in=ncsc_in,
                                  out_prefix=args.output_prefix)
 
+    refined_xyz = args.output_prefix+model_format
+    modify_output(refined_xyz, file_info.get("inscode_mods"))
+    
     # Expand sym here
     if has_ncsc:
-        refined_xyz = args.output_prefix+model_format
         logger.write("Expanding {}".format(refined_xyz))
         st, cif_ref = utils.fileio.read_structure_from_pdb_and_mmcif(refined_xyz)
         st.expand_ncs(gemmi.HowToNameCopiedChain.Short)

servalcat/utils/model.py
@@ -11,6 +11,8 @@ from servalcat.utils import restraints
 import gemmi
 import numpy
 import os
+import itertools
+import string
 
 def shake_structure(st, sigma):
     print("Randomizing structure with rmsd of {}".format(sigma))
@@ -343,3 +345,120 @@ def all_B(st):
 
     return ret
 # all_B()
+
+def microheterogeneity_for_refmac(st, monlib):
+    st.setup_entities()
+    topo = gemmi.prepare_topology(st, monlib)
+    mh_res = []
+    chains = []
+    icodes = {} # to avoid overlaps
+    modifications = [] # return value
+    
+    # Check if microheterogeneity exists
+    for chain in st[0]:
+        for rg in chain.get_polymer().residue_groups():
+            if len(rg) > 1:
+                ress = [r for r in rg]
+                chains.append(chain.name)
+                mh_res.append(ress)
+                ress_str = "/".join([str(r) for r in ress])
+                logger.write("Microheterogeneity detected in chain {}: {}".format(chain.name, ress_str))
+
+    if not mh_res: return []
+
+    for chain in st[0]:
+        for res in chain:
+            if res.seqid.icode != " ":
+                icodes.setdefault(chain.name, {}).setdefault(res.seqid.num, []).append(res.seqid.icode)
+                
+    def append_links(rinfo, prr, toappend):
+        for pbond in filter(lambda f: (f.provenance==gemmi.Provenance.PrevLink and
+                                       f.rkind==gemmi.RKind.Bond),
+                            rinfo.forces):
+            atoms = topo.bonds[pbond.index].atoms
+            assert len(atoms) == 2
+            found = None
+            for i in range(2):
+                if any(filter(lambda ra: atoms[i]==ra, prr)): found = i
+            if found is not None:
+                toappend.append([atoms[i], atoms[1-i]]) # prev atom, current atom
+    # append_links()
+    
+    mh_res_all = sum(mh_res, [])
+    mh_link = {}
+
+    # Check links
+    for cinfo in topo.chain_infos:
+        for rinfo in cinfo.res_infos:
+            # If this residue is microheterogeneous
+            if rinfo.res in mh_res_all:
+                for pr in rinfo.prev:
+                    prr = pr.get(rinfo).res
+                    mh_link.setdefault(rinfo.res, []).append([prr, "prev", pr.link, []])
+                    append_links(rinfo, prr, mh_link[rinfo.res][-1][-1])
+                    
+            # Check if previous residue(s) is microheterogeneous
+            for pr in rinfo.prev:
+                prr = pr.get(rinfo).res
+                if prr in mh_res_all:
+                    mh_link.setdefault(prr, []).append([rinfo.res, "next", pr.link, []])
+                    append_links(rinfo, prr, mh_link[prr][-1][-1])
+
+    # Change IDs
+    for chain_name, rr in zip(chains, mh_res):
+        chars = string.ascii_uppercase
+        # avoid already used inscodes
+        if chain_name in icodes and rr[0].seqid.num in icodes[chain_name]:
+            used_codes = set(icodes[chain_name][rr[0].seqid.num])
+            chars = list(filter(lambda x: x not in used_codes, chars))
+        for ir, r in enumerate(rr[1:]):
+            modifications.append((chain_name, r.seqid.num, r.seqid.icode, chars[ir]))
+            r.seqid.icode = chars[ir]
+
+    # Update connections (LINKR)
+    for chain_name, rr in zip(chains, mh_res):
+        for r in rr:
+            for p in mh_link.get(r, []):
+                for atoms in p[-1]:
+                    con = gemmi.Connection()
+                    con.asu = gemmi.Asu.Same
+                    con.type = gemmi.ConnectionType.Covale
+                    con.link_id = p[2]
+                    if p[1] == "prev":
+                        p1 = gemmi.AtomAddress(chain_name, p[0].seqid, p[0].name, atoms[1].name, atoms[1].altloc)
+                        p2 = gemmi.AtomAddress(chain_name, r.seqid, r.name, atoms[0].name, atoms[0].altloc)
+                    else:
+                        p1 = gemmi.AtomAddress(chain_name, r.seqid, r.name, atoms[1].name, atoms[1].altloc)
+                        p2 = gemmi.AtomAddress(chain_name, p[0].seqid, p[0].name, atoms[0].name, atoms[0].altloc)
+                        
+                    con.partner1 = p1
+                    con.partner2 = p2
+                    logger.write("Adding link: {}".format(con))
+                    st.connections.append(con)
+        for r1, r2 in itertools.combinations(rr, 2):
+            for a1 in set([a.altloc for a in r1]):
+                for a2 in set([a.altloc for a in r2]):
+                    con = gemmi.Connection()
+                    con.asu = gemmi.Asu.Same
+                    con.link_id = "gap"
+                    # XXX altloc will be ignored when atom does not match.. grrr
+                    con.partner1 = gemmi.AtomAddress(chain_name, r1.seqid, r1.name, "", a1)
+                    con.partner2 = gemmi.AtomAddress(chain_name, r2.seqid, r2.name, "", a2)
+                    st.connections.append(con)
+
+    return modifications
+# microheterogeneity_for_refmac()
+
+def modify_inscodes_back(st, modifications):
+    mods = dict([((cname,num,newcode), icode)for cname,num,icode,newcode in modifications])
+    logger.write("Modifications to be modified back: {}".format(mods))
+    for chain in st[0]:
+        for res in chain:
+            key = (chain.name, res.seqid.num, res.seqid.icode)
+            if key in mods:
+                logger.write(" Changing inscode '{}' to '{}' for {}/{}".format(res.seqid.icode,
+                                                                           mods[key],
+                                                                           chain.name,
+                                                                           res.seqid.num))
+                res.seqid.icode = mods[key]
+# modify_inscodes_back()

servalcat/spa/sfcalc.py
@@ -49,6 +49,9 @@ def add_sfcalc_args(parser):
                         help='Keep 1st model only')
     parser.add_argument('--no_sharpen_before_mask', action='store_true',
                         help='By default half maps are sharpened before masking by std of signal and unsharpened after masking. This option disables it.')
+    parser.add_argument('--no_fix_microheterogeneity', action='store_true', 
+                        help='By default it will fix microheterogeneity for Refmac')
+
 # add_sfcalc_args()
 
 def add_arguments(parser):
@@ -214,6 +217,12 @@ def main(args):
         st.cell = unit_cell
         st.spacegroup_hm = "P 1"
 
+        if not args.no_fix_microheterogeneity:
+            # TODO need to check external restraints
+            monlib = utils.restraints.load_monomer_library(st[0].get_all_residue_names()) # FIXME should use user provided libraries
+            mhtr_mods = utils.model.microheterogeneity_for_refmac(st, monlib)
+            ret["inscode_mods"] = mhtr_mods
+            
         model_format = utils.fileio.check_model_format(args.model)
         chain_id_len_max = max([len(x) for x in utils.model.all_chain_ids(st)])
         if chain_id_len_max > 1 and model_format == ".pdb":

commit 30098cba0baff8b9b2c5ff287a535a713544287d

fixed a bug introduced in 99654bba - matching _entry.id does not work for pdb and mmcif; pdb always uses uppercase letters - no need to read/write files if no modification requested

servalcat/spa/run_refmac.py
@@ -153,7 +153,7 @@ $$""")
 # calc_fsc()
 
 def modify_output(filename, inscode_mods=None):
-    if inscode_mods is None: return
+    if not inscode_mods: return
     
     st, cif_ref = utils.fileio.read_structure_from_pdb_and_mmcif(filename)
     utils.model.modify_inscodes_back(st, inscode_mods)

servalcat/utils/fileio.py
@@ -52,7 +52,12 @@ def write_mmcif(st, cif_out, cif_ref=None):
             logger.write("  Give up using cif reference.")
             return write_mmcif(st, cif_out)
             
-        block = doc.find_block(st_new.info["_entry.id"])
+        blocks = list(filter(lambda b: b.find_loop("_atom_site.id"), doc))
+        if len(blocks) == 0:
+            logger.write("No _atom_site found in {}".format(e))
+            logger.write("  Give up using cif reference.")
+            return write_mmcif(st, cif_out)
+        block = blocks[0]
         # to remove fract_transf_matrix. maybe we should keep some (like _atom_sites.solution_hydrogens)?
         # we do not want this because cell may be updated
         block.find_mmcif_category("_atom_sites.").erase()

servalcat/utils/model.py
@@ -451,7 +451,7 @@ def microheterogeneity_for_refmac(st, monlib):
 
 def modify_inscodes_back(st, modifications):
     mods = dict([((cname,num,newcode), icode)for cname,num,icode,newcode in modifications])
-    logger.write("Modifications to be modified back: {}".format(mods))
+    logger.write("Insertion codes to be modified back: {}".format(mods))
     for chain in st[0]:
         for res in chain:
             key = (chain.name, res.seqid.num, res.seqid.icode)