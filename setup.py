75f6237d0e789c936f379666d2c9aef739f5c8ea
Keitaro Yamashita <keitaroyam@users.noreply.github.com>
2021-05-26 10:43:56 +0100

    changed required pandas version for python2 compatibility

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='servalcat',
     description= 'Structure refinement and validation for crystallography and single particle analysis',
     license='MPL-2.0',
     packages=setuptools.find_packages(),
-    install_requires=['numpy>=1.15','scipy','pandas>=1.0.0','gemmi>=0.4.6'],
+    install_requires=['numpy>=1.15','scipy','pandas>=0.24.2','gemmi>=0.4.6'],
     entry_points={
       'console_scripts': [
           'servalcat = servalcat.command_line:main',
