keitaroyam/servalcat

commit f0721daa689df09eb1c3a73ed5ac1eda28d15444

add an option to keep map origin for mtz file (no shift needed for model) default behavior unchanged (except some bug fixes). in refine_spa/sfcalc: meaning of --no_shift is changed, and its old behavior is taken over by --no_trim fofc: --crop renamed to --trim. --trim_mtz added

servalcat/__init__.py
@@ -6,4 +6,4 @@ This software is released under the
 Mozilla Public License, version 2.0; see LICENSE.
 """
 
-__version__ = '0.1.60'
+__version__ = '0.1.61'

servalcat/spa/run_refmac.py
@@ -58,6 +58,7 @@ def add_arguments(parser):
     parser.add_argument('--shake_radius', default=0.3,
                         help='Shake rmsd in case of --cross_validation_method=shake')
     parser.add_argument('--mask_for_fofc', help="Mask file for Fo-Fc map calculation")
+    parser.add_argument('--trim_fofc_mtz', action="store_true", help="diffmap.mtz will have smaller cell (if --mask_for_fofc is given)")
     parser.add_argument("--monlib",
                         help="Monomer library path. Default: $CLIBD_MON")
     parser.add_argument("--fsc_resolution", type=float,
@@ -301,9 +302,10 @@ def main(args):
     st, cif_ref = utils.fileio.read_structure_from_pdb_and_mmcif(refmac_prefix+model_format)
     utils.model.adp_analysis(st)
     
-    if not args.no_shift:
+    if not args.no_trim:
         st.cell = maps[0][0].unit_cell
-        spa.shiftback.shift_back_model(st, file_info["shifts"]) # st is modified
+        if not args.no_shift:
+            spa.shiftback.shift_back_model(st, file_info["shifts"]) # st is modified
     
     modify_output(st, file_info.get("inscode_mods"))
     utils.fileio.write_model(st, prefix=args.output_prefix,
@@ -335,9 +337,10 @@ def main(args):
         
         # Modify output
         st_sr, cif_ref_sr = utils.fileio.read_structure_from_pdb_and_mmcif(refmac_prefix_shaken+model_format)
-        if not args.no_shift:
+        if not args.no_trim:
             st_sr.cell = maps[0][0].unit_cell
-            spa.shiftback.shift_back_model(st_sr, file_info["shifts"])
+            if not args.no_shift:
+                spa.shiftback.shift_back_model(st_sr, file_info["shifts"])
             
         modify_output(st_sr, file_info.get("inscode_mods"))
         utils.fileio.write_model(st_sr, prefix=args.output_prefix+"_shaken_refined",
@@ -378,7 +381,8 @@ def main(args):
                                                           half1_only=(args.cross_validation and args.cross_validation_method == "throughout"))
         spa.fofc.write_files(hkldata, map_labs, maps[0][1], stats_str,
                              mask=mask, output_prefix="diffmap",
-                             crop=mask is not None, normalize_map=mask is not None)
+                             trim_map=mask is not None, trim_mtz=args.trim_fofc_mtz,
+                             normalize_map=mask is not None)
     else:
         logger.write("Will not calculate Fo-Fc map because half maps were not provided")
 

servalcat/spa/fofc.py
@@ -12,6 +12,7 @@ import numpy
 import time
 from servalcat.utils import logger
 from servalcat import utils
+from servalcat.spa import shift_maps
 import argparse
 
 def add_arguments(parser):
@@ -34,8 +35,10 @@ def add_arguments(parser):
                         help="Just for debugging purpose: turn off FSC-based weighting")
     parser.add_argument("--sharpening_b", type=float,
                         help="Use B value (negative value for sharpening) instead of standard deviation of the signal")
-    parser.add_argument("--crop", action='store_true',
-                        help="Write cropped maps")
+    parser.add_argument("--trim", action='store_true',
+                        help="Write trimmed maps")
+    parser.add_argument("--trim_mtz", action='store_true',
+                        help="Write trimmed mtz")
     parser.add_argument("--monlib",
                         help="Monomer library path. Default: $CLIBD_MON")
     parser.add_argument("--omit_proton", action='store_true',
@@ -268,9 +271,23 @@ def calc_fofc(st, d_min, maps, mask=None, monlib=None, B=None, half1_only=False,
 # calc_fofc()
 
 def write_files(hkldata, map_labs, grid_start, stats_str,
-                mask=None, output_prefix="diffmap", crop=False, normalize_map=False, omit_h_electron=False):
+                mask=None, output_prefix="diffmap", trim_map=False, trim_mtz=False,
+                normalize_map=False, omit_h_electron=False):
     # this function may modify the overall scale of FWT/DELFWT.
 
+    if mask is not None and (trim_map or trim_mtz):
+        new_cell, new_shape, grid_start, shifts = shift_maps.determine_shape_and_shift(mask=gemmi.FloatGrid(mask,
+                                                                                                            hkldata.cell,
+                                                                                                            hkldata.sg),
+                                                                                       grid_start=grid_start,
+                                                                                       padding=5,
+                                                                                       mask_cutoff=0.5,
+                                                                                       noncentered=True,
+                                                                                       noncubic=True,
+                                                                                       json_out=None)
+    else:
+        new_cell, new_shape, shifts = None, None, None
+        
     if normalize_map and mask is not None:
         logger.write("Normalized Fo-Fc map requested.")
         delfwt_map = hkldata.fft_map("DELFWT", grid_size=mask.shape)
@@ -296,8 +313,7 @@ def write_files(hkldata, map_labs, grid_start, stats_str,
             filename = "{}_normalized_fofc.mrc".format(output_prefix)
         logger.write("  Writing {}".format(filename))
         utils.maps.write_ccp4_map(filename, scaled, cell=hkldata.cell,
-                                  mask_for_extent=mask if crop else None,
-                                  grid_start=grid_start)
+                                  grid_start=grid_start, grid_shape=new_shape)
 
         # Write Fo map as well
         if "FWT" in hkldata.df:
@@ -310,9 +326,20 @@ def write_files(hkldata, map_labs, grid_start, stats_str,
             filename = "{}_normalized_fo.mrc".format(output_prefix)
             logger.write("  Writing {}".format(filename))
             utils.maps.write_ccp4_map(filename, scaled, cell=hkldata.cell,
-                                      mask_for_extent=mask if crop else None,
-                                      grid_start=grid_start)
+                                      grid_start=grid_start, grid_shape=new_shape)
 
+    if trim_mtz and shifts is not None:
+        hkldata2 = utils.hkl.HklData(new_cell, hkldata.sg, anomalous=False, df=None)
+        d_min = hkldata.d_min_max()[0]
+        for lab in map_labs + ["FP", "FC"]:
+            gr = hkldata.fft_map(lab, mask.shape)
+            gr = gemmi.FloatGrid(gr.get_subarray(*(list(grid_start)+list(new_shape))),
+                                 new_cell, hkldata.sg)
+            ad = gemmi.transform_map_to_f_phi(gr).prepare_asu_data(dmin=d_min)
+            hkldata2.merge_asu_data(ad, lab)
+            hkldata2.translate(lab, -shifts)
+        hkldata = hkldata2
+        
     dump_to_mtz(hkldata, map_labs, "{}.mtz".format(output_prefix))
     open("{}_Fstats.log".format(output_prefix), "w").write(stats_str)
 # write_files()
@@ -383,7 +410,7 @@ def main(args):
                                              omit_h_electron=args.omit_h_electron)
     write_files(hkldata, map_labs, grid_start, stats_str,
                 mask=mask, output_prefix=args.output_prefix,
-                crop=args.crop, normalize_map=args.normalized_map, omit_h_electron=args.omit_h_electron)
+                trim_map=args.trim, trim_mtz=args.trim_mtz, normalize_map=args.normalized_map, omit_h_electron=args.omit_h_electron)
     
 # main()
 

servalcat/spa/sfcalc.py
@@ -36,9 +36,12 @@ def add_sfcalc_args(parser):
     parser.add_argument('--resolution',
                         type=float,
                         help='')
+    parser.add_argument('--no_trim',
+                        action='store_true',
+                        help='Keep original box (not recommended)')
     parser.add_argument('--no_shift',
                         action='store_true',
-                        help='Use original box (not recommended)')
+                        help='Keep map origin so that output maps overlap with the input maps.')
     parser.add_argument('--blur',
                         nargs="+", # XXX probably no need to be multiple
                         type=float,
@@ -58,6 +61,13 @@ def add_sfcalc_args(parser):
     parser.add_argument('--no_fix_microheterogeneity', action='store_true', 
                         help='By default it will fix microheterogeneity for Refmac')
 # add_sfcalc_args()
+
+"""
+Possible choices:
+ - no mask => no shift AND no trim
+ - mask AND trim => shift OR no shift
+ - mask AND no trim => no shift
+"""
 
 def add_arguments(parser):
     parser.description = 'Structure factor calculation for Refmac'
@@ -218,9 +228,16 @@ def main(args, monlib=None):
         if not args.no_shift:
             logger.write("WARNING: setting --no_shift because --no_mask is given")
             args.no_shift = True
+        if not args.no_trim:
+            logger.write("WARNING: setting --no_trim because --no_mask is given")
+            args.no_trim = True
         if args.mask:
             logger.write("WARNING: Your --mask is ignored because --no_mask is given")
             args.mask = None
+    elif args.no_trim and not args.no_shift:
+        logger.write("WARNING: setting --no_shift because --no_trim is given (and --no_mask not given)")
+        args.no_shift = True
+        
 
     if args.resolution is None and args.model and utils.fileio.splitext(args.model)[1].endswith("cif"):
         doc = gemmi.cif.read(args.model)
@@ -337,7 +354,7 @@ def main(args, monlib=None):
             ret["ncsc_file"] = "ncsc.txt"
         
         st_new = st.clone()
-        if len(st.ncs) > 0 and not args.no_shift:
+        if len(st.ncs) > 0:
             utils.model.expand_ncs(st)
             logger.write(" Saving expanded model: input_model_expanded.*")
             utils.fileio.write_model(st, "input_model_expanded", pdb=True, cif=True)
@@ -358,7 +375,7 @@ def main(args, monlib=None):
         
     if args.no_shift:
         logger.write(" Saving input model with unit cell information")
-        utils.fileio.write_model(st, "starting_model", pdb=True, cif=True)
+        utils.fileio.write_model(st_new, "starting_model", pdb=True, cif=True)
         ret["model_file"] = "starting_model" + model_format
 
     if mask:
@@ -379,7 +396,7 @@ def main(args, monlib=None):
             if b_before_mask is None: b_before_mask = determine_b_before_mask(st, maps, grid_start, mask, resolution)                
             maps = utils.maps.sharpen_mask_unsharpen(maps, mask, resolution, b=b_before_mask)
 
-        if not args.no_shift:
+        if not args.no_trim:
             logger.write(" Shifting maps and/or model..")
             if args.padding is None: args.padding = args.mask_radius * 2
             new_cell, new_shape, starts, shifts = shift_maps.determine_shape_and_shift(mask=mask,
@@ -391,17 +408,19 @@ def main(args, monlib=None):
                                                                                        json_out="shifts.json")
             ret["shifts"] = shifts
             vol_mask = numpy.count_nonzero(numpy.array(mask)>0.5)
-            vol_map = new_shape[0] * new_shape[1] * new_shape[2]
+            vol_map = new_shape[0] * new_shape[1] * new_shape[2] # XXX assuming all orthogonal
             ret["vol_ratio"] = vol_mask/vol_map
             logger.write(" Vol_mask/Vol_map= {:.2e}".format(ret["vol_ratio"]))
             
             if st_new:
-                for cra in st_new[0].all():
-                    cra.atom.pos += shifts
-                
                 st_new.cell = new_cell
                 st_new.spacegroup_hm = "P 1"
-                if len(st_new.ncs) > 0:
+
+                if not args.no_shift:
+                    for cra in st_new[0].all():
+                        cra.atom.pos += shifts
+                
+                if not args.no_shift and len(st_new.ncs) > 0:
                     new_ops = utils.symmetry.apply_shift_for_ncsops(st_new.ncs, shifts)
                     st_new.ncs.clear()
                     st_new.ncs.extend(new_ops)
@@ -412,9 +431,9 @@ def main(args, monlib=None):
                     utils.symmetry.write_symmetry_expanded_model(st_new, "{}_expanded".format(args.shifted_model_prefix),
                                                                  pdb=True, cif=True)
 
-                logger.write(" Saving shifted model..")
+                logger.write(" Saving model in trimmed map..")
                 utils.fileio.write_model(st_new, args.shifted_model_prefix, pdb=True, cif=True)
-                ret["model_file"] = args.shifted_model_prefix + model_format
+                ret["model_file"] = args.shifted_model_prefix + model_format # the same name whether --no_shift given or not
 
             logger.write(" Trimming maps..")
             for i in range(len(maps)): # Update maps
@@ -447,14 +466,19 @@ def main(args, monlib=None):
         map_labs = ["Fout"]
         sig_lab = None
 
-    if args.no_shift:
-        logger.write("Saving original maps as mtz files..") # XXX not always original (can be masked!)
+    if args.no_mask:
+        logger.write("Saving unmasked maps as mtz file..")
         mtzout = args.output_mtz_prefix+".mtz"
     else:
-        logger.write(" Saving masked maps as mtz files..") # XXX not always masked (if no mask..)
+        logger.write(" Saving masked maps as mtz file..")
         mtzout = args.output_masked_prefix+"_obs.mtz"
 
     hkldata.df.rename(columns=dict(F_map1="Fmap1", F_map2="Fmap2", FP="Fout"), inplace=True)
+    if "shifts" in ret and args.no_shift:
+        for lab in map_labs: # apply phase shift
+            logger.write("  applying phase shift for {} with translation {}".format(lab, -ret["shifts"]))
+            hkldata.translate(lab, -ret["shifts"])
+        
     write_map_mtz(hkldata, mtzout,
                   map_labs=map_labs, sig_lab=sig_lab, blurs=args.blur)
     ret["mtz_file"] = mtzout

servalcat/utils/hkl.py
@@ -84,11 +84,14 @@ class HklData:
     # copy()
 
     def merge_asu_data(self, asu_data, label, common_only=True):
-        if label in self.df:
+        if self.df is not None and label in self.df:
             raise Exception("Duplicated label")
         
         df_tmp = df_from_asu_data(asu_data, label)
-        if common_only:
+
+        if self.df is None:
+            self.df = df_tmp
+        elif common_only:
             self.df = self.df.merge(df_tmp)
         else:
             self.df = self.df.merge(other.df, how="outer")
@@ -357,3 +360,14 @@ class HklData:
         else:
             return k1, B1
     # scale_k_and_b()
+
+    def translate(self, lab, shift):
+        # apply phase shift
+        assert numpy.iscomplexobj(self.df[lab])
+        
+        if type(shift) != gemmi.Position:
+            shift = gemmi.Position(*shift)
+            
+        self.df[lab] *= numpy.exp(2.j*numpy.pi*numpy.dot(self.miller_array(),
+                                                         self.cell.fractionalize(shift).tolist()))
+    # translate()

commit 39c9c5d5f1a327c6dc42bed0edc66858801dadd8

fixed a bug introduced in f0721daa

servalcat/__init__.py
@@ -6,4 +6,4 @@ This software is released under the
 Mozilla Public License, version 2.0; see LICENSE.
 """
 
-__version__ = '0.1.65'
+__version__ = '0.1.66'

servalcat/spa/run_refmac.py
@@ -263,10 +263,10 @@ def main(args):
     if has_ncsc:
         args.keyword_file.append(file_info["ncsc_file"])
 
-    if not args.no_shift:
+    if not args.no_trim:
         refmac_prefix = "{}_{}".format(args.shifted_model_prefix, args.output_prefix)
     else:
-        refmac_prefix = args.output_prefix
+        refmac_prefix = args.output_prefix # XXX this should be different name (nomask etc?)
 
     # Weight auto scale
     if args.weight_matrix is None and args.weight_auto_scale is None: