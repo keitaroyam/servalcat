keitaroyam/servalcat

commit f67e3932c01c9e753989dcaab78ab790b803073f

remove redundant symmetry handling code

servalcat/__init__.py
@@ -6,4 +6,4 @@ This software is released under the
 Mozilla Public License, version 2.0; see LICENSE.
 """
 
-__version__ = '0.2.85'
+__version__ = '0.2.86'

servalcat/utils/symmetry.py
@@ -15,6 +15,55 @@ from servalcat.utils import fileio
 from servalcat.utils import model
 from servalcat.utils import generate_operators
 
+def add_symmetry_args(parser, require_pg=False):
+    parser.add_argument('--pg', required=require_pg, help="Point group symbol")
+    parser.add_argument('--twist', type=float, help="Helical twist (degree)")
+    parser.add_argument('--rise', type=float, help="Helical rise (Angstrom)")
+    parser.add_argument('--center', type=float, nargs=3, help="Origin of symmetry. Default: center of the box")
+    parser.add_argument('--axis1', type=float, nargs=3, help="Axis1 (if I: 5-fold, O: 4-fold, T: 3-fold)")
+    parser.add_argument('--axis2', type=float, nargs=3, help="Axis2 (if I: 5-fold, O: 4-fold, T: 3-fold)")
+# add_symmetry_args()
+    
+def update_ncs_from_args(args, st, map_and_start=None, filter_model_helical_contacting=False):
+    is_helical = args.twist is not None
+    if not is_helical and not args.pg:
+        if len(st.ncs) > 0:
+            logger.write("Strict NCS detected from model.")
+            show_ncs_operators_axis_angle(st.ncs)
+        return
+    
+    if len(st.ncs) > 0:
+        logger.write(" WARNING: NCS information in model file will be ignored")
+    
+    if map_and_start is not None:
+        start_xyz = numpy.array(map_and_start[0].get_position(*map_and_start[1]).tolist())
+    else:
+        start_xyz = numpy.zeros(3)
+
+    if args.center is None:
+        A = numpy.array(st.cell.orthogonalization_matrix.tolist())
+        center = numpy.sum(A, axis=1) / 2 #+ start_xyz
+        logger.write("Center: {}".format(center))
+    else:
+        center = numpy.array(args.center)
+        
+    if is_helical:
+        ncsops = generate_helical_operators(st, start_xyz, center,
+                                            args.pg, args.twist, args.rise,
+                                            axis1=args.axis1, axis2=args.axis2)
+        logger.write("{} helical operators found".format(len(ncsops)))
+        if filter_model_helical_contacting:
+            utils.model.filter_helical_contacting(st)
+    else:
+        _, _, ops = operators_from_symbol(args.pg, axis1=args.axis1, axis2=args.axis2)
+        logger.write("{} operators found for {}".format(len(ops), args.pg))
+        show_operators_axis_angle(ops)
+        ncsops = make_NcsOps_from_matrices(ops, cell=st.cell, center=center)
+
+    st.ncs.clear()
+    st.ncs.extend([x for x in ncsops if not x.tr.is_identity()])
+# ncsops_from_args()
+
 def get_matrices_using_relion(sym):
     ps = subprocess.check_output(["relion_refine", "--sym", sym.strip(), "--print_symmetry_ops"])
 

servalcat/spa/sfcalc.py
@@ -48,10 +48,7 @@ def add_sfcalc_args(parser):
                         nargs="+", # XXX probably no need to be multiple
                         type=float,
                         help='Sharpening or blurring B')
-    parser.add_argument('--pg',
-                        help="Point group symbol for strict symmetry. The coordinate system is consitent with RELION.")
-    parser.add_argument('--twist', type=float, help="Helical twist (degree)")
-    parser.add_argument('--rise', type=float, help="Helical rise (Angstrom)")
+    utils.symmetry.add_symmetry_args(parser) # add --pg etc
     parser.add_argument('--ignore_symmetry',
                         help='Ignore symmetry information (MTRIX/_struct_ncs_oper) in the model file')
     parser.add_argument('--keep_multiple_models', action='store_true', 
@@ -230,8 +227,6 @@ def main(args, monlib=None):
     if (args.twist, args.rise).count(None) == 1:
         raise SystemExit("ERROR: give both helical paramters --twist and --rise")
 
-    is_helical = args.twist is not None
-
     if args.no_mask:
         args.mask_radius = None
         if not args.no_shift:
@@ -348,31 +343,8 @@ def main(args, monlib=None):
                 for i in reversed(range(len(st.ncs))):
                     if st.ncs[i].given: del st.ncs[i]
 
-        if is_helical:
-            ops = utils.symmetry.generate_helical_operators(st, start_xyz, center,
-                                                            args.pg, args.twist, args.rise)
-            logger.write("{} helical operators found".format(len(ops)))
-            st.ncs.clear()
-            st.ncs.extend([x for x in ops if not x.tr.is_identity()])
-            #ret["helical"] = ops
-            utils.model.filter_helical_contacting(st)
-        elif args.pg:
-            logger.write("Point group symmetry: {}".format(args.pg))
-            if len(st.ncs) > 0:
-                logger.write(" WARNING: NCS information in model file will be ignored")
-
-            _, _, ops = utils.symmetry.operators_from_symbol(args.pg)
-            if ops:
-                logger.write(" {} operators found".format(len(ops)))
-                ops = utils.symmetry.make_NcsOps_from_matrices(ops, cell=unit_cell, center=center)
-                st.ncs.clear()
-                st.ncs.extend([x for x in ops if not x.tr.is_identity()])
-
-        elif len(st.ncs) > 0:
-            logger.write("Strict NCS detected from model.")
-
+        utils.symmetry.update_ncs_from_args(args, st, map_and_start=maps[0], filter_model_helical_contacting=True)
         st_new = st.clone()
-
         if len(st.ncs) > 0:
             if not args.no_check_ncs_overlaps and utils.model.check_symmetry_related_model_duplication(st):
                 raise SystemExit("\nError: Too many symmetery-related contacts detected.\n"

servalcat/utils/commands.py
@@ -37,12 +37,8 @@ def add_arguments(p):
     group = parser.add_mutually_exclusive_group()
     group.add_argument('--map', help="Take box size from the map")
     group.add_argument('--cell', type=float, nargs=6, help="Box size")
-    parser.add_argument('--pg', required=True, help="Point group symbol")
-    parser.add_argument('--twist', type=float, help="Helical twist (degree)")
-    parser.add_argument('--rise', type=float, help="Helical rise (Angstrom)")
-    parser.add_argument('--center', type=float, nargs=3, help="Origin of symmetry. Default: center of the box")
-    parser.add_argument('--axis1', type=float, nargs=3, help="Axis1 (if I: 5-fold, O: 4-fold, T: 3-fold)")
-    parser.add_argument('--axis2', type=float, nargs=3, help="Axis2 (if I: 5-fold, O: 4-fold, T: 3-fold)")
+    sym_group = parser.add_argument_group("symmetry")
+    symmetry.add_symmetry_args(sym_group, require_pg=True)
     parser.add_argument('--chains', nargs="*", action="append", help="Select chains to keep")
     parser.add_argument('--howtoname', choices=["dup", "short", "number"], default="short",
                         help="How to decide new chain IDs in expanded model (default: short); "
@@ -173,16 +169,13 @@ def symmodel(args):
         logger.error("ERROR: give both helical paramters --twist and --rise")
         return
 
-    is_helical = args.twist is not None
-
     st, cif_ref = fileio.read_structure_from_pdb_and_mmcif(args.model)
     st.spacegroup_hm = "P 1"
-    start_xyz = numpy.zeros(3)
+    map_and_start = None
     if args.map:
         logger.write("Reading cell from map")
-        g, grid_start = fileio.read_ccp4_map(args.map)
-        st.cell = g.unit_cell
-        start_xyz = numpy.array(g.get_position(*grid_start).tolist())
+        map_and_start = fileio.read_ccp4_map(args.map)
+        st.cell = map_and_start[0].unit_cell
     elif args.cell:
         st.cell = gemmi.UnitCell(*args.cell)
     elif not st.cell.is_crystal():
@@ -198,36 +191,19 @@ def symmodel(args):
 
     all_chains = [c.name for c in st[0] if c.name not in st[0]]
 
-    if args.center is None:
-        A = numpy.array(st.cell.orthogonalization_matrix.tolist())
-        center = numpy.sum(A, axis=1) / 2 #+ start_xyz
-        logger.write("Center: {}".format(center))
-    else:
-        center = numpy.array(args.center)
-
-    if is_helical:
-        ncsops = symmetry.generate_helical_operators(st, start_xyz, center,
-                                                     args.pg, args.twist, args.rise,
-                                                     axis1=args.axis1, axis2=args.axis2)
-        logger.write("{} helical operators found".format(len(ncsops)))
-    else:
-        _, _, ops = symmetry.operators_from_symbol(args.pg, axis1=args.axis1, axis2=args.axis2)
-        logger.write("{} operators found for {}".format(len(ops), args.pg))
-        symmetry.show_operators_axis_angle(ops)
-        ncsops = symmetry.make_NcsOps_from_matrices(ops, cell=st.cell, center=center)
-
-    st.ncs.clear()
-    st.ncs.extend([x for x in ncsops if not x.tr.is_identity()])
+    symmetry.update_ncs_from_args(args, st, map_and_start=map_and_start)
 
     if args.biomt:
         st.assemblies.clear()
         st.raw_remarks = []
         a = gemmi.Assembly("1")
         g = gemmi.Assembly.Gen()
-        for i, nop in enumerate(ncsops):
+        if sum(map(lambda x: x.tr.is_identity(), st.ncs)) == 0:
+            g.operators.append(gemmi.Assembly.Operator()) # add identity
+        for i, nop in enumerate(st.ncs):
             op = gemmi.Assembly.Operator()
             op.transform = nop.tr
-            if not nop.tr.is_identity(): op.type = "point symmetry operation"
+            if not nop.tr.is_identity(): op.type = "point symmetry operation" # XXX if helical?
             g.operators.append(op)
         g.chains = all_chains
         a.generators.append(g)

commit 0e6fa770157ba44800ed4c3c655eec9a9f88f8ff

helical symmetry: fixed a bug introduced in f67e3932

servalcat/__init__.py
@@ -6,4 +6,4 @@ This software is released under the
 Mozilla Public License, version 2.0; see LICENSE.
 """
 
-__version__ = '0.2.98'
+__version__ = '0.2.99'

servalcat/utils/symmetry.py
@@ -52,8 +52,6 @@ def update_ncs_from_args(args, st, map_and_start=None, filter_model_helical_cont
                                             args.pg, args.twist, args.rise,
                                             axis1=args.axis1, axis2=args.axis2)
         logger.write("{} helical operators found".format(len(ncsops)))
-        if filter_model_helical_contacting:
-            model.filter_helical_contacting(st)
     else:
         _, _, ops = operators_from_symbol(args.pg, axis1=args.axis1, axis2=args.axis2)
         logger.write("{} operators found for {}".format(len(ops), args.pg))
@@ -62,6 +60,9 @@ def update_ncs_from_args(args, st, map_and_start=None, filter_model_helical_cont
 
     st.ncs.clear()
     st.ncs.extend([x for x in ncsops if not x.tr.is_identity()])
+
+    if is_helical and filter_model_helical_contacting:
+        model.filter_helical_contacting(st)
 # ncsops_from_args()
 
 def get_matrices_using_relion(sym):