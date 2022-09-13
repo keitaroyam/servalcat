keitaroyam/servalcat

commit bdc7d8aeb5b33bef7fc079ed70f9e0f8fb534458

fofc: sharpen Fo-Fc map using sigma_U,T instead of sigma_T

servalcat/__init__.py
@@ -6,4 +6,4 @@ This software is released under the
 Mozilla Public License, version 2.0; see LICENSE.
 """
 
-__version__ = '0.2.119'
+__version__ = '0.2.120'

servalcat/spa/fofc.py
@@ -139,7 +139,7 @@ def calc_maps(hkldata, B=None, has_halfmaps=True, half1_only=False, no_fsc_weigh
     logger.write("Calculating maps..")
     logger.write(" sharpening method: ", end="")
     if sharpening_b is None:
-        logger.write("1/sqrt(FSC * Mn(Fo))")
+        logger.write("1/sqrt(FSC * Mn(Fo)) for Fo and 1/sigma_U,T for Fo-Fc")
     else:
         logger.write("1/exp(-B*s^2/4) with B= {:.2f}".format(sharpening_b))
 
@@ -189,15 +189,16 @@ def calc_maps(hkldata, B=None, has_halfmaps=True, half1_only=False, no_fsc_weigh
 
         if sharpening_b is None:
             k = sig_fo * numpy.sqrt(fsc)
+            k_fofc = numpy.sqrt(S) if S > 0 else 1. # to avoid zero-division. if S=0 then w=0.
         else:
             k = numpy.exp(-sharpening_b*s2_bin/4)
             
         lab_suf = "" if B is None else "_b0"
         if has_halfmaps:
-            tmp["FWT"+lab_suf][idxes] = w_nomodel/k*Fo
+            tmp["FWT"+lab_suf][idxes] = w_nomodel / k * Fo
             if has_fc:
-                tmp["DELFWT"+lab_suf][idxes] = delfwt/k
-                tmp["Fupdate"+lab_suf][idxes] = fup/k
+                tmp["DELFWT"+lab_suf][idxes] = delfwt / k_fofc
+                tmp["Fupdate"+lab_suf][idxes] = fup / k
         elif has_fc:
             tmp["DELFWT"+lab_suf][idxes] = delfwt
 
@@ -210,7 +211,7 @@ def calc_maps(hkldata, B=None, has_halfmaps=True, half1_only=False, no_fsc_weigh
             if has_fc:
                 S_l = S * k2_l
                 w = 1. if no_fsc_weights or not has_halfmaps else S_l/(S_l+varn)            
-                delfwt = (Fo-D*Fc)*w/k/k_l
+                delfwt = (Fo-D*Fc)*w/k_fofc/k_l
                 fup = (w*Fo+(1.-w)*D*Fc)/k/k_l
                 logger.write("{:4d} {:.4e} {:.4e} {:.4e} {:.4e} {:.4e} {:.4e}".format(i_bin,
                                                                                       numpy.average(k),

commit 1ecd214c9bc0513517636c6d120e2467dc45321f

fixed a bug introduced in bdc7d8ae

servalcat/__init__.py
@@ -6,4 +6,4 @@ This software is released under the
 Mozilla Public License, version 2.0; see LICENSE.
 """
 
-__version__ = '0.2.120'
+__version__ = '0.2.121'

servalcat/spa/fofc.py
@@ -189,7 +189,7 @@ def calc_maps(hkldata, B=None, has_halfmaps=True, half1_only=False, no_fsc_weigh
 
         if sharpening_b is None:
             k = sig_fo * numpy.sqrt(fsc)
-            k_fofc = numpy.sqrt(S) if S > 0 else 1. # to avoid zero-division. if S=0 then w=0.
+            k_fofc = numpy.sqrt(S) if has_fc and S > 0 else 1. # to avoid zero-division. if S=0 then w=0.
         else:
             k = numpy.exp(-sharpening_b*s2_bin/4)
             