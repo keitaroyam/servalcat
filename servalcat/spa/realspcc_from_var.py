a70356007422e30797b97daa30ec31242e835115
Keitaro Yamashita <keitaroyam@users.noreply.github.com>
2022-10-03 20:19:19 +0100

    realspcc_from_var: refactoring. use Simpson's rule for integration

--- servalcat/spa/realspcc_from_var.py
+++ servalcat/spa/realspcc_from_var.py
@@ -23,7 +23,7 @@ def add_arguments(parser):
     parser.add_argument("-d", '--resolution', type=float, required=True)
     parser.add_argument('-m', '--mask', help="mask file")
     parser.add_argument('-w', '--weight', help="weight")
-    parser.add_argument('-f', help="noise, signal, total")
+    parser.add_argument('-f', choices=["noise", "signal", "total"], required=True)
     parser.add_argument('--sharpen_signal', action="store_true", help="")
     parser.add_argument('--x_max', type=float, default=20)
     parser.add_argument("-B", type=float, help="Sharpening (negative)/blurring (positive) B value")
@@ -38,9 +38,10 @@ def parse_args(arg_list):
     return parser.parse_args(arg_list)
 # parse_args()
 
-def f_noise(s, x, s_list, w_list, B):
-    tinv = numpy.exp(-B*s**2/2) if B is not None else 1.
-    return s**2 * tinv * numpy.interp(s, s_list, w_list) * numpy.sinc(2*numpy.abs(s)*numpy.abs(x))
+def integ_var(x, s_list, w_list, B):
+    tinv = numpy.exp(-B*s_list**2/2) if B is not None else 1.
+    return scipy.integrate.simps(s_list**2 * tinv * w_list * numpy.sinc(2*numpy.abs(s_list)*numpy.abs(x)),
+                                 s_list)
 
 def calc_var(hkldata, kind="noise", sharpen_signal=False, weight=None):
     wsq = None
@@ -71,26 +72,19 @@ def find_b(hkldata, smax, x, kind="noise", sharpen_signal=False, weight=None): #
     logger.write("kind= {} sharpen_signal={}, weight={}".format(kind, sharpen_signal, weight))
     wsq = calc_var(hkldata, kind, sharpen_signal, weight)
     for B in -numpy.arange(0,100,5):
-        cov_xx = scipy.integrate.quad(f_noise, 0, smax, args=(0, bin_s, wsq, B))[0]
-        cov_xy = scipy.integrate.quad(f_noise, 0, smax, args=(x, bin_s, wsq, B))[0]
+        cov_xx = integ_var(0., bin_s, wsq, B)
+        cov_xy = integ_var(x, bin_s, wsq, B)
         print(B, cov_xy/cov_xx)
 # find_b()        
 
-def calc_cc_from_var(hkldata, smax, x_max=20, x_step=0.1, kind="noise", sharpen_signal=False, weight=None, B=None):
-    assert 1./hkldata.binned_df.d_min.iloc[-1] <= smax
+def calc_cc_from_var(hkldata, x_list, kind="noise", sharpen_signal=False, weight=None, B=None):
     bin_s = 0.5*(1./hkldata.binned_df[["d_min", "d_max"]]).sum(axis=1).to_numpy() # 0.5 * (1/d_max + 1/d_min)
-
     logger.write("kind= {} sharpen_signal={}, weight={}".format(kind, sharpen_signal, weight))
     wsq = calc_var(hkldata, kind, sharpen_signal, weight)
-    cov_xx = scipy.integrate.quad(f_noise, 0, smax, args=(0, bin_s, wsq, B))[0]
-    x_all = numpy.arange(0, x_max, x_step)
-    cc_all = []
-    for x in x_all:
-        cov_xy = scipy.integrate.quad(f_noise, 0, smax, args=(x, bin_s, wsq, B))[0]
-        cc_all.append(cov_xy/cov_xx)
-
-    cc_all = numpy.array(cc_all)
-    return x_all, cc_all
+    cov_xx = integ_var(0., bin_s, wsq, B)
+    cov_xy = integ_var(x_list[:,None], bin_s, wsq, B)
+    cc_all = cov_xy / cov_xx
+    return cc_all
 # calc_cc_from_var()
 
 def main(args):
@@ -109,9 +103,10 @@ def main(args):
         find_b(hkldata, smax, args.find_B_at,
                kind=args.f, sharpen_signal=args.sharpen_signal, weight=args.weight)
     else:
-        x_all, cc_all = calc_cc_from_var(hkldata, smax, x_max=args.x_max, kind=args.f,
-                                         sharpen_signal=args.sharpen_signal, weight=args.weight,
-                                         B=args.B)
+        x_all = numpy.arange(0, args.x_max, 0.1)
+        cc_all = calc_cc_from_var(hkldata, x_list=x_all, kind=args.f,
+                                  sharpen_signal=args.sharpen_signal, weight=args.weight,
+                                  B=args.B)
 
         ofs = open("{}.dat".format(args.output_prefix), "w")
         ofs.write("# smax= {}\n".format(smax))
