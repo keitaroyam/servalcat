959f2d0021a1e7b22fb69697112ae00f6b67021c
Keitaro Yamashita <keitaroyam@users.noreply.github.com>
2022-12-30 17:26:04 +0900

    tests: add refine_cx

--- tests/test_xtal.py
+++ tests/test_xtal.py
@@ -24,6 +24,17 @@ from servalcat import command_line
 root = os.path.abspath(os.path.dirname(__file__))
 
 class XtalTests(unittest.TestCase):
+    def setUp(self):
+        self.wd = tempfile.mkdtemp(prefix="servaltest_")
+        os.chdir(self.wd)
+        print("In", self.wd)
+    # setUp()
+    
+    def tearDown(self):
+        os.chdir(root)
+        shutil.rmtree(self.wd)
+    # tearDown()
+
     def test_scale(self):
         mtzin = os.path.join(root, "5e5z", "5e5z.mtz.gz")
         pdbin = os.path.join(root, "5e5z", "5e5z.pdb.gz")
@@ -49,20 +60,39 @@ class XtalTests(unittest.TestCase):
         os.remove("sigmaa.mtz")
 
         numpy.testing.assert_allclose(hkldata.binned_df.d_min,
-                                      [5.0834,3.6631,3.0103,2.6155,2.3440,2.1426,1.9855,1.8586,1.7532,1.6640],
-                                      rtol=1e-5)
+                                      [5.0834, 3.6631, 3.0103, 2.6155, 2.344 , 2.1426, 1.9855, 1.8586,
+                                       1.7532, 1.664 ],
+                                      rtol=1e-4)
         numpy.testing.assert_allclose(hkldata.binned_df.D0,
-                                      [0.844600,0.983733,1.022023,0.965324,0.996962,1.009091,0.997227,0.989490,0.937605,0.928479],
+                                      [0.844599, 0.983733, 1.022031, 0.965314, 0.996966, 1.009102,
+                                       0.997227, 0.98949 , 0.937605, 0.928479],
                                       rtol=1e-5)
         numpy.testing.assert_allclose(hkldata.binned_df.D1,
-                                      [0.297493,0.000004,0.000081,0.000075,0.996606,0.999243,1.000051,0.999999,1.000000,1.000001],
+                                      [2.974686e-01, 5.625804e-08, 2.500706e-10, 3.981426e-10,
+                                       7.635458e-07, 8.245066e-08, 1.000051e+00, 9.999994e-01,
+                                       9.999997e-01, 1.000001e+00],
                                       rtol=1e-5)
         numpy.testing.assert_allclose(hkldata.binned_df.S,
-                                      [ 84.857641, 95.794294, 68.853409, 96.124315, 74.231890,110.065386,116.446683, 95.913027, 81.069189, 43.255406],
+                                      [ 84.857332,  95.794274,  68.854357,  96.124265,  74.228905,
+                                        110.075772, 116.446679,  95.912967,  81.069191,  43.255408],
                                       rtol=1e-5)
 
     # test_sigmaa()
-        
+
+    def test_refine_cx(self):
+        mtzin = os.path.join(root, "biotin", "biotin_talos.mtz")
+        pdbin = os.path.join(root, "biotin", "biotin_talos.pdb")
+        sys.argv = ["", "refine_cx", "--model", pipes.quote(pdbin),
+                    "--hklin", pipes.quote(mtzin), "--bref", "iso_then_aniso"]
+        command_line.main()
+        self.assertTrue(os.path.isfile("refined_2_aniso.mtz"))
+        r_factor = None
+        for l in open("refined_2_aniso.log"):
+            if l.startswith("           R factor"):
+                r_factor = float(l.split()[-1])
+        self.assertLess(r_factor, 0.185)
+    # test_refine_cx()
+    
 # class XtalTests
 
 if __name__ == '__main__':
