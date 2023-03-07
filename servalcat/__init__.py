2149dec21fe20f78cdb129001d3c25a94d12c70e
Keitaro Yamashita <keitaroyam@users.noreply.github.com>
2023-03-07 15:01:12 +0000

    refine_geom: --update_dictionary now does preset refinement for acedrg

--- servalcat/__init__.py
+++ servalcat/__init__.py
@@ -6,5 +6,5 @@ This software is released under the
 Mozilla Public License, version 2.0; see LICENSE.
 """
 
-__version__ = '0.4.0'
-__date__ = '2023-03-06'
+__version__ = '0.4.1'
+__date__ = '2023-03-07'
--- servalcat/refine/refine_geom.py
+++ servalcat/refine/refine_geom.py
@@ -28,7 +28,7 @@ def add_arguments(parser):
                         help="restraint dictionary cif file(s)")
     parser.add_argument('--ncycle', type=int, default=10,
                         help="number of CG cycles (default: %(default)d)")
-    parser.add_argument('--hydrogen', default="all", choices=["all", "yes", "no", "full"],
+    parser.add_argument('--hydrogen', default="all", choices=["all", "yes", "no"],
                         help="all: add riding hydrogen atoms, yes: use hydrogen atoms if present, no: remove hydrogen atoms in input. "
                         "Default: %(default)s")
     parser.add_argument('--randomize', type=float, default=0,
@@ -47,91 +47,118 @@ def parse_args(arg_list):
     return parser.parse_args(arg_list)
 # parse_args()
 
+def refine_and_update_dictionary(cif_in, monomer_dir, output_prefix, randomize=0, ncycle1=10, ncycle2=30):
+    doc = gemmi.cif.read(cif_in)
+    for block in doc: # this block will be reused below
+        st = gemmi.make_structure_from_chemcomp_block(block)
+        if len(st) > 0: break
+    else:
+        raise SystemExit("No model in the cif file")
+    monlib = utils.restraints.load_monomer_library(st, monomer_dir=monomer_dir, # monlib is needed for ener_lib
+                                                   cif_files=[cif_in],
+                                                   stop_for_unknowns=True)
+    try:
+        topo = utils.restraints.prepare_topology(st, monlib, h_change=gemmi.HydrogenChange.ReAdd,
+                                                 check_hydrogen=False)
+    except RuntimeError as e:
+        raise SystemExit("Error: {}".format(e))
+
+    geom = Geom(st, topo, monlib, shake_rms=randomize)
+    refiner = Refine(st, geom)
+    logger.writeln("Running {} cycles with wchir=4 wvdw=2".format(ncycle1))
+    geom.calc_kwds["wchir"] = 4
+    geom.calc_kwds["wvdw"] = 2
+    refiner.run_cycles(ncycle1)
+    logger.writeln("Running {} cycles with wchir=1 wvdw=2".format(ncycle2))
+    geom.calc_kwds["wchir"] = 1
+    refiner.run_cycles(ncycle2)
+
+    # replace xyz
+    pos = {cra.atom.name: cra.atom.pos.tolist() for cra in refiner.st[0].all()}
+    for row in block.find("_chem_comp_atom.", ["atom_id", "x", "y", "z"]):
+        p = pos[row.str(0)]
+        for i in range(3):
+            row[i+1] = "{:.3f}".format(p[i])
+    # add description
+    loop = block.find_loop("_pdbx_chem_comp_description_generator.comp_id").get_loop()
+    if not loop:
+        loop = block.init_loop("_pdbx_chem_comp_description_generator.", ["comp_id",
+                                                                          "program_name",
+                                                                          "program_version",
+                                                                          "descriptor"])
+    tags = [x[x.index(".")+1:] for x in loop.tags]
+    row = ["" for _ in range(len(tags))]
+    for tag, val in (("comp_id", st[0][0][0].name),
+                     ("program_name", "servalcat"),
+                     ("program_version", servalcat.__version__),
+                     ("descriptor", "optimization tool")):
+        if tag in tags: row[tags.index(tag)] = val
+    loop.add_row(gemmi.cif.quote_list(row))
+    doc.write_file(output_prefix + "_updated.cif", style=gemmi.cif.Style.Aligned)
+# refine_and_update_dictionary()
+
+def refine_geom(model_in, monomer_dir, cif_files, h_change, ncycle, output_prefix, randomize, refmac_keywords):
+    st = utils.fileio.read_structure(model_in)
+    utils.model.setup_entities(st, clear=True, force_subchain_names=True)
+    st.assign_cis_flags()
+    if st.ncs:
+        st2 = st.clone()
+        logger.writeln("Take NCS constraints into account.")
+        st2.expand_ncs(gemmi.HowToNameCopiedChain.Dup)
+        utils.fileio.write_model(st2, file_name="input_expanded.pdb")
+
+    monlib = utils.restraints.load_monomer_library(st, monomer_dir=monomer_dir,
+                                                   cif_files=cif_files,
+                                                   stop_for_unknowns=True)
+    utils.restraints.find_and_fix_links(st, monlib) # should remove unknown id here?
+    try:
+        topo = utils.restraints.prepare_topology(st, monlib, h_change=h_change,
+                                                 check_hydrogen=(h_change==gemmi.HydrogenChange.NoChange))
+    except RuntimeError as e:
+        raise SystemExit("Error: {}".format(e))
+    geom = Geom(st, topo, monlib, shake_rms=randomize, refmac_keywords=refmac_keywords)
+    refiner = Refine(st, geom)
+    stats = refiner.run_cycles(ncycle)
+    utils.fileio.write_model(refiner.st, output_prefix, pdb=True, cif=True)
+    with open(output_prefix + "_stats.json", "w") as ofs:
+        for s in stats: s["geom"] = s["geom"].to_dict()
+        json.dump(stats, ofs, indent=2)
+        logger.writeln("Refinement statistics saved: {}".format(ofs.name))
+# refine_geom()
+
 def main(args):
+    keywords = []
+    if args.keywords: keywords = sum(args.keywords, [])
+    if args.keyword_file: keywords.extend(l for f in sum(args.keyword_file, []) for l in open(f))
+    decide_prefix = lambda f: utils.fileio.splitext(os.path.basename(f))[0] + "_refined"
     if args.model:
         if not args.output_prefix:
-            args.output_prefix = utils.fileio.splitext(os.path.basename(args.model))[0] + "_refined"
-        st = utils.fileio.read_structure(args.model)
-        utils.model.setup_entities(st, clear=True, force_subchain_names=True)
-        st.assign_cis_flags()
-        if st.ncs:
-            st2 = st.clone()
-            logger.writeln("Take NCS constraints into account.")
-            st2.expand_ncs(gemmi.HowToNameCopiedChain.Dup)
-            utils.fileio.write_model(st2, file_name="input_expanded.pdb")
-
-        if args.ligand: args.ligand = sum(args.ligand, [])
-        monlib = utils.restraints.load_monomer_library(st, monomer_dir=args.monlib, cif_files=args.ligand,
-                                                       stop_for_unknowns=True)
-        utils.restraints.find_and_fix_links(st, monlib) # should remove unknown id here?
+            args.output_prefix = decide_prefix(args.model)
+        if args.ligand:
+            args.ligand = sum(args.ligand, [])
         h_change = {"all":gemmi.HydrogenChange.ReAddButWater,
                     "full":gemmi.HydrogenChange.ReAdd,
                     "yes":gemmi.HydrogenChange.NoChange,
                     "no":gemmi.HydrogenChange.Remove}[args.hydrogen]
+        refine_geom(model_in=args.model,
+                    monomer_dir=args.monlib,
+                    cif_files=args.ligand,
+                    h_change=h_change,
+                    ncycle=args.ncycle,
+                    output_prefix=args.output_prefix,
+                    randomize=args.randomize,
+                    refmac_keywords=keywords)
     else:
         if not args.output_prefix:
-            args.output_prefix = utils.fileio.splitext(os.path.basename(args.update_dictionary))[0] + "_refined"
-        doc = gemmi.cif.read(args.update_dictionary)
-        for block in doc: # this block will be reused below
-            st = gemmi.make_structure_from_chemcomp_block(block)
-            if len(st) > 0: break
-        else:
-            raise SystemExit("No model in the cif file")
+            args.output_prefix = decide_prefix(args.update_dictionary)
         if args.ligand:
             logger.writeln("WARNING: monlib and ligand are ignored in the dictionary updating mode")
-            args.ligand = [args.update_dictionary]
-        monlib = utils.restraints.load_monomer_library(st, monomer_dir=args.monlib, # monlib is needed for ener_lib
-                                                       cif_files=[args.update_dictionary],
-                                                       stop_for_unknowns=True)
-        h_change = gemmi.HydrogenChange.NoChange
-        
-    try:
-        topo = utils.restraints.prepare_topology(st, monlib, h_change=h_change,
-                                                 check_hydrogen=(args.hydrogen=="yes"))
-    except RuntimeError as e:
-        raise SystemExit("Error: {}".format(e))
-
-    if args.hydrogen == "full":
-        for cra in st[0].all():
-            if cra.atom.is_hydrogen(): cra.atom.occ = 1.
-
-    keywords = []
-    if args.keywords or args.keyword_file:
-        if args.keywords: keywords = sum(args.keywords, [])
-        if args.keyword_file: keywords.extend(l for f in sum(args.keyword_file, []) for l in open(f))
-
-    geom = Geom(st, topo, monlib, shake_rms=args.randomize, refmac_keywords=keywords)
-    refiner = Refine(st, geom)
-    stats = refiner.run_cycles(args.ncycle)
-    utils.fileio.write_model(refiner.st, args.output_prefix, pdb=True, cif=True)
-    with open(args.output_prefix + "_stats.json", "w") as ofs:
-        for s in stats: s["geom"] = s["geom"].to_dict()
-        json.dump(stats, ofs, indent=2)
-        logger.writeln("Refinement statistics saved: {}".format(ofs.name))
-
-    if args.update_dictionary:
-        # replace xyz
-        pos = {cra.atom.name: cra.atom.pos.tolist() for cra in refiner.st[0].all()}
-        for row in block.find("_chem_comp_atom.", ["atom_id", "x", "y", "z"]):
-            p = pos[row.str(0)]
-            for i in range(3):
-                row[i+1] = "{:.3f}".format(p[i])
-        # add description
-        loop = block.find_loop("_pdbx_chem_comp_description_generator.comp_id").get_loop()
-        if not loop:
-            loop = block.init_loop("_pdbx_chem_comp_description_generator.", ["comp_id",
-                                                                              "program_name",
-                                                                              "program_version",
-                                                                              "descriptor"])
-        tags = [x[x.index(".")+1:] for x in loop.tags]
-        row = ["" for _ in range(len(tags))]
-        for tag, val in (("comp_id", st[0][0][0].name),
-                         ("program_name", "servalcat"),
-                         ("program_version", servalcat.__version__),
-                         ("descriptor", "optimization tool")):
-            if tag in tags: row[tags.index(tag)] = val
-        loop.add_row(gemmi.cif.quote_list(row))
-        doc.write_file(args.output_prefix + "_updated.cif", style=gemmi.cif.Style.Aligned)
+        if keywords:
+            logger.writeln("WARNING: refmac keywords are ignored in the dictionary updating mode")
+        refine_and_update_dictionary(cif_in=args.update_dictionary,
+                                     monomer_dir=args.monlib,
+                                     output_prefix=args.output_prefix,
+                                     randomize=args.randomize)
 # main()
 
 if __name__ == "__main__":
