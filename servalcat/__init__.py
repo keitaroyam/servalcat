8ca98dc43be76223dbe9a6eb73a875fa97560ad5
Keitaro Yamashita <keitaroyam@users.noreply.github.com>
2023-04-20 17:41:52 +0100

    requires gemmi 0.6.1

--- servalcat/__init__.py
+++ servalcat/__init__.py
@@ -6,5 +6,5 @@ This software is released under the
 Mozilla Public License, version 2.0; see LICENSE.
 """
 
-__version__ = '0.4.13'
-__date__ = '2023-04-14'
+__version__ = '0.4.14'
+__date__ = '2023-04-20'
