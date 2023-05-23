c761e8b6ba4f5f873e850b59270c30f75c09dafa
Keitaro Yamashita <keitaroyam@users.noreply.github.com>
2023-05-23 16:24:11 +0100

    write_mmcif: bug fix; cif_ref may not have _entry.id

--- servalcat/utils/fileio.py
+++ servalcat/utils/fileio.py
@@ -104,7 +104,7 @@ def write_mmcif(st, cif_out, cif_ref=None):
         # we do not want this because cell may be updated
         block.find_mmcif_category("_atom_sites.").erase()
         st_new.update_mmcif_block(block, groups)
-        st_new.info["_entry.id"] = st_new.info["_entry.id"][:78]
+        if "_entry.id" in st_new.info: st_new.info["_entry.id"] = st_new.info["_entry.id"][:78]
         doc.write_file(cif_out, style=gemmi.cif.Style.Aligned)
     else:
         st_new.name = st_new.name[:78] # this will become _entry.id
