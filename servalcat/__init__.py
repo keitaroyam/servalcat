keitaroyam/servalcat

commit 15d4fef42ba059bdb55440ce5bb294497e376882

refmacat: fix for 3 letter chain IDs Refmac only reads first 4 letters from label_asym_id so ABC_p style label will go wrong. Refmac does not seem to need _p suffixes

servalcat/refmac/refmac_wrapper.py
@@ -74,7 +74,9 @@ def prepare_crd(st, crdout, ligand, make, monlib_path=None, h_pos="elec",
     for chain in st[0]:
         if not chain.name:
             chain.name = "X" # Refmac behavior. Empty chain name will cause a problem
-    gemmi.setup_for_crd(st)
+        for res in chain:
+            if res.is_water():
+                res.name = "HOH"
 
     # TODO read dictionary from xyzin (priority: user cif -> monlib -> xyzin
     try:
@@ -147,7 +149,10 @@ def prepare_crd(st, crdout, ligand, make, monlib_path=None, h_pos="elec",
             if not tmp[3:5].isdigit():
                 tmp = "XX"
         st.info[date_key] = tmp
-
+    # For > 2 letter chain IDs. It invalidates _struct_asym, but Refmac does not need it actually
+    for chain in st[0]:
+        for res in chain:
+            res.subchain = chain.name
     doc = gemmi.prepare_refmac_crd(st, topo, monlib, h_change)
     doc.write_file(crdout, style=gemmi.cif.Style.NoBlankLines)
     logger.writeln("crd file written: {}".format(crdout))
@@ -193,7 +198,7 @@ def modify_output(pdbout, cifout, fixes, hout, cispeps, keep_original_output=Fal
         # should we check metals and put MetalC?
 
     # fix entity (Refmac seems to make DNA non-polymer; as seen in 1fix)
-    utils.model.setup_entities(st, clear=True, overwrite_entity_type=True)
+    utils.model.setup_entities(st, clear=True, overwrite_entity_type=True, force_subchain_names=True)
     for e in st.entities:
         if not e.full_sequence and e.entity_type == gemmi.EntityType.Polymer and e.subchains:
             rspan = st[0].get_subchain(e.subchains[0])

commit 210300c79963d47d271ba28ace6e1b992a4ba5fb

refmacat: 15d4fef4 caused non-polymer part to be included in local NCS alignment

servalcat/__init__.py
@@ -6,5 +6,5 @@ This software is released under the
 Mozilla Public License, version 2.0; see LICENSE.
 """
 
-__version__ = '0.4.59'
-__date__ = '2024-02-17'
+__version__ = '0.4.60'
+__date__ = '2024-02-26'

servalcat/refmac/refmac_wrapper.py
@@ -149,10 +149,18 @@ def prepare_crd(st, crdout, ligand, make, monlib_path=None, h_pos="elec",
             if not tmp[3:5].isdigit():
                 tmp = "XX"
         st.info[date_key] = tmp
-    # For > 2 letter chain IDs. It invalidates _struct_asym, but Refmac does not need it actually
+    # internal chain ID
     for chain in st[0]:
         for res in chain:
-            res.subchain = chain.name
+            if len(chain.name) < 3:
+                # Change Axp (or AAxp) to A_p (or AA_p)
+                res.subchain = res.subchain[:-2] + "_" + res.subchain[-1:]
+            else:
+                # Refmac only expects '_' at 2nd or 3rd position, and can accept up to 4 letters.
+                # Using raw chain ID may change alignment result for local NCS restraints,
+                # but to avoid this we would need chain ID translation, which is too complicated.
+                # This also invalidates _struct_asym, which Refmac does not seem to care
+                res.subchain = chain.name
     doc = gemmi.prepare_refmac_crd(st, topo, monlib, h_change)
     doc.write_file(crdout, style=gemmi.cif.Style.NoBlankLines)
     logger.writeln("crd file written: {}".format(crdout))

servalcat/spa/run_refmac.py
@@ -823,8 +823,7 @@ def main(args):
 
     # Modify output
     st, cif_ref = utils.fileio.read_structure_from_pdb_and_mmcif(refmac_prefix+model_format)
-    st.entities.clear()
-    st.setup_entities()
+    utils.model.setup_entities(st, clear=True, overwrite_entity_type=True, force_subchain_names=True)
 
     if not args.no_trim:
         st.cell = maps[0][0].unit_cell
@@ -856,7 +855,9 @@ def main(args):
         logger.writeln("In this refinement, hydrogen is removed regardless of --hydrogen option")
         if use_gemmi_prep:
             xyzin = refmac_prefix + ".crd"
-            prepare_crd(utils.fileio.read_structure(refmac_prefix+model_format),
+            st_tmp = utils.fileio.read_structure(refmac_prefix+model_format)
+            utils.model.setup_entities(st_tmp, clear=True, overwrite_entity_type=True, force_subchain_names=True)
+            prepare_crd(st_tmp,
                         crdout=xyzin, ligand=[refmac_prefix+model_format],
                         make={"hydr":"n"},
                         fix_long_resnames=False) # we do not need output file - do we?
@@ -883,7 +884,9 @@ def main(args):
             logger.writeln("Cross validation: 2nd run with hydrogen")
             if use_gemmi_prep:
                 xyzin = refmac_prefix_shaken + ".crd"
-                prepare_crd(utils.fileio.read_structure(refmac_prefix_shaken+model_format),
+                st_tmp = utils.fileio.read_structure(refmac_prefix_shaken+model_format)
+                utils.model.setup_entities(st_tmp, clear=True, overwrite_entity_type=True, force_subchain_names=True)
+                prepare_crd(st_tmp,
                             crdout=xyzin, ligand=[refmac_prefix+model_format],
                             make={"hydr":"a"},
                             fix_long_resnames=False) # we do not need output file - do we?
@@ -901,8 +904,7 @@ def main(args):
         
         # Modify output
         st_sr, cif_ref_sr = utils.fileio.read_structure_from_pdb_and_mmcif(refmac_prefix_shaken+model_format)
-        st_sr.entities.clear()
-        st_sr.setup_entities()
+        utils.model.setup_entities(st_sr, clear=True, overwrite_entity_type=True, force_subchain_names=True)
         if not args.no_trim:
             st_sr.cell = maps[0][0].unit_cell
             st_sr.setup_cell_images()