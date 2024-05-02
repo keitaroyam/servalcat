keitaroyam/servalcat

commit 5bd8d321f4d13eebace8efbddf2e99bf01d63551

refine: torsion angle control now understands refmac keywords "restr tors incl" and excl

servalcat/refine/refine.py
@@ -52,9 +52,6 @@ class Geom:
             numpy.random.seed(0)
             utils.model.shake_structure(self.st, shake_rms, copy=False)
             utils.fileio.write_model(self.st, "shaken", pdb=True, cif=True)
-        if not self.unrestrained:
-            self.geom.load_topo(topo)
-            self.check_chemtypes(os.path.join(monlib.path(), "ener_lib.cif"), topo)
         self.use_nucleus = use_nucleus
         self.calc_kwds = {"use_nucleus": self.use_nucleus}
         if params:
@@ -63,9 +60,16 @@ class Geom:
                 if k in params:
                     self.calc_kwds[k] = params[k]
                     logger.writeln("setting geometry weight {}= {}".format(k, params[k]))
+            inc_tors, exc_tors = utils.restraints.make_torsion_rules(params.get("restr", {}))
+            rtors = utils.restraints.select_restrained_torsions(monlib, inc_tors, exc_tors)
+            self.geom.mon_tors_names = rtors["monomer"]
+            self.geom.link_tors_names = rtors["link"]
             self.group_occ = GroupOccupancy(self.st, params.get("occu"))
         else:
             self.group_occ = GroupOccupancy(self.st, None)
+        if not self.unrestrained:
+            self.geom.load_topo(topo)
+            self.check_chemtypes(os.path.join(monlib.path(), "ener_lib.cif"), topo)
         self.geom.finalize_restraints()
         self.outlier_sigmas = dict(bond=5, angle=5, torsion=5, vdw=5, ncs=5, chir=5, plane=5, staca=5, stacd=5, per_atom=5)
         self.parents = {}

servalcat/refine/refine_geom.py
@@ -142,7 +142,8 @@ def refine_geom(model_in, monomer_dir, cif_files, h_change, ncycle, output_prefi
 
     monlib = utils.restraints.load_monomer_library(st, monomer_dir=monomer_dir,
                                                    cif_files=cif_files,
-                                                   stop_for_unknowns=True)
+                                                   stop_for_unknowns=True,
+                                                   params=params)
     utils.restraints.find_and_fix_links(st, monlib, add_found=find_links) # should remove unknown id here?
     try:
         topo, metal_kws = utils.restraints.prepare_topology(st, monlib, h_change=h_change,

servalcat/refine/refine_spa.py
@@ -132,7 +132,7 @@ def main(args):
     st = utils.fileio.read_structure(args.model)
     try:
         monlib = utils.restraints.load_monomer_library(st, monomer_dir=args.monlib, cif_files=args.ligand,
-                                                       stop_for_unknowns=True)
+                                                       stop_for_unknowns=True, params=params)
     except RuntimeError as e:
         raise SystemExit("Error: {}".format(e))
     if not args.keep_entities:

servalcat/refine/refine_xtal.py
@@ -153,7 +153,7 @@ def main(args):
     else:
         try:
             monlib = utils.restraints.load_monomer_library(st, monomer_dir=args.monlib, cif_files=args.ligand,
-                                                           stop_for_unknowns=True)
+                                                           stop_for_unknowns=True, params=params)
         except RuntimeError as e:
             raise SystemExit("Error: {}".format(e))
         utils.model.setup_entities(st, clear=True, force_subchain_names=True, overwrite_entity_type=True)

servalcat/refmac/refmac_keywords.py
@@ -385,7 +385,87 @@ def read_occupancy_params(l, r):
 
     return r
 # read_occupancy_params()
-        
+
+def read_restr_params(l, r):
+    s = l.split()
+
+    def read_tors_params(itk):
+        ret = {"flag": True}
+        if s[itk].lower().startswith("none"): # remove all
+            ret["flag"] = False
+            itk += 1
+        elif s[itk].lower().startswith("resi"):
+            ret["residue"] = s[itk+1]
+            itk += 2
+        elif s[itk].lower().startswith("grou"):
+            ret["group"] = s[itk+1] # group_name_tors_restr_o
+            itk += 2
+        elif s[itk].lower().startswith("link"):
+            ret["link"] = s[itk+1] #link_name_tors_restr_o
+            itk += 2
+        else:
+            pass # raise error?
+        while itk < len(s):
+            if s[itk].lower().startswith("name"):
+                itk += 1
+                ret["tors_name"] = s[itk] # RES_NAME_TORS_NAME_O
+            elif s[itk].lower().startswith("valu"):
+                itk += 1
+                ret["tors_value"] = float(s[itk]) # RES_NAME_TORS_VALUE_O
+            elif s[itk].lower().startswith("sigm"):
+                itk += 1
+                ret["tors_sigma"] = float(s[itk]) # RES_NAME_TORS_SIGMA_O
+            elif s[itk].lower().startswith("peri"):
+                itk += 1
+                ret["tors_period"] = int(s[itk]) # RES_NAME_TORS_PERIOD_O
+            itk += 1
+        return ret, itk
+    # read_tors_params()
+    
+    if not s[0].lower().startswith("rest"):
+        return r
+    if s[1].lower().startswith("excl"):
+        # restr_params.f90 subroutine exclude_restraints
+        #r["exclude"] = True
+        pass
+    elif s[1].lower().startswith("conf"): # not implemented in Refmac
+        pass
+    elif s[1].lower().startswith(("bp", "pair", "base")):
+        if s[2].lower().startswith("dist"):
+            r["plane_dist"] = True
+        else:
+            r["basepair"] = True
+            # TODO read dnarna_params.txt
+    elif s[1].lower().startswith("tors") and len(s) > 2:
+        itk = 2
+        if s[itk].lower().startswith("hydr"):
+            itk += 1
+            if s[itk].lower().startswith("incl"):
+                itk += 1
+                r["htors_restraint"] = True
+                if s[itk].lower().startswith("all"): # this is servalcat default for now
+                    r["htors_restraint_type"] = "all"
+                elif s[itk].lower().startswith("sele"):
+                    r["htors_restraint_type"] = "sele"
+                    r["htors_restraint_list"] = s[itk+1:]
+            else:
+                itk += 1
+                r["htors_restraint"] = False
+        elif s[itk].lower().startswith("fbas"):
+            pass # set user_basepair_file
+        elif s[itk].lower().startswith("incl"):
+            tmp, itk = read_tors_params(itk+1)
+            r.setdefault("torsion_include", []).append(tmp)
+        elif s[itk].lower().startswith("excl"):
+            # Should warn if value/sigma/period given?
+            tmp, itk = read_tors_params(itk+1)
+            r.setdefault("torsion_exclude", []).append(tmp)
+    elif s[1].lower().startswith("resi"):
+        pass
+    elif s[1].lower().startswith("chir"):
+        pass
+# read_restr_params()
+
 def read_make_params(l, r):
     # TODO: hout,ribo,valu,spec,form,sdmi,segi
     s = l.split()
@@ -437,7 +517,7 @@ def parse_line(l, ret):
     if ntok == 0: return
     if s[0].lower().startswith("exte"):
         ret.setdefault("exte", []).append(read_exte(s))
-    if s[0].lower().startswith("make"):
+    elif s[0].lower().startswith("make"):
         read_make_params(l, ret.setdefault("make", {}))
     elif s[0].lower().startswith(("sour", "scat")):
         k = s[1].lower()
@@ -470,6 +550,8 @@ def parse_line(l, ret):
         read_ridge_params(l, ret.setdefault("ridge", {}))
     elif s[0].lower().startswith("occu"):
         read_occupancy_params(l, ret.setdefault("occu", {}))
+    elif s[0].lower().startswith("rest"):
+        read_restr_params(l, ret.setdefault("restr", {}))
     elif s[0].lower().startswith("angl") and ntok > 1:
         ret["wangle"] = float(s[1])
     elif s[0].lower().startswith("tors") and ntok > 1:

servalcat/utils/commands.py
@@ -758,7 +758,7 @@ def geometry(args):
         st.remove_hydrogens()
     try:
         monlib = restraints.load_monomer_library(st, monomer_dir=args.monlib, cif_files=args.ligand, 
-                                                 stop_for_unknowns=True)
+                                                 stop_for_unknowns=True, params=params)
     except RuntimeError as e:
         raise SystemExit("Error: {}".format(e))
 

servalcat/utils/restraints.py
@@ -16,6 +16,7 @@ import random
 import numpy
 import pandas
 import json
+import fnmatch
 
 default_proton_scale = 1.13 # scale of X-proton distance to X-H(e) distance
 
@@ -72,7 +73,8 @@ def rename_cif_modification_if_necessary(doc, known_ids):
 # rename_cif_modification_if_necessary()
 
 def load_monomer_library(st, monomer_dir=None, cif_files=None, stop_for_unknowns=False,
-                         ignore_monomer_dir=False, update_old_atom_names=True):
+                         ignore_monomer_dir=False, update_old_atom_names=True,
+                         params=None):
     resnames = st[0].get_all_residue_names()
 
     if monomer_dir is None and not ignore_monomer_dir:
@@ -147,6 +149,9 @@ def load_monomer_library(st, monomer_dir=None, cif_files=None, stop_for_unknowns
 
     if update_old_atom_names:
         logger.write(monlib.update_old_atom_names(st))
+
+    if params:
+        update_torsions(monlib, params.get("restr", {}).get("torsion_include", {}))
     
     return monlib
 # load_monomer_library()
@@ -163,6 +168,154 @@ def fix_elements_in_model(monlib, st):
                     logger.writeln(f"WARNING: correcting element of {st[0].get_cra(at)} to {el.name}")
                     at.element = el
 # correct_elements_in_model()
+
+def read_group(s): # can use gemmi.ChemComp.read_group when gemmi 0.6.6 is out
+    s = s.lower()
+    d = {"non-": gemmi.ChemComp.Group.NonPolymer,
+         ("pept", "l-pe"): gemmi.ChemComp.Group.Peptide,
+         "p-pe": gemmi.ChemComp.Group.PPeptide,
+         "m-pe": gemmi.ChemComp.Group.MPeptide,
+         "dna/": gemmi.ChemComp.Group.DnaRna,
+         "dna": gemmi.ChemComp.Group.Dna,
+         "rna": gemmi.ChemComp.Group.Rna,
+         "pyra": gemmi.ChemComp.Group.Pyranose,
+         "keto": gemmi.ChemComp.Group.Ketopyranose,
+         "fura": gemmi.ChemComp.Group.Furanose,
+         }
+    for x in d:
+        if s.startswith(x):
+            return d[x]
+    return gemmi.ChemComp.Group.Null
+# read_group()
+
+def update_torsions(monlib, params):
+    # take subset
+    params = [p for p in params
+              if any(x in p for x in ("tors_value", "tors_sigma", "tors_period"))]
+    if not params:
+        return
+    logger.writeln("Updating torsion targets in dictionaries")
+    for p in params:
+        if "residue" in p:
+            tors = [cc.rt.torsions for cc in monlib.monomers.values()
+                    if fnmatch.fnmatch(cc.name, p["residue"])]
+        elif "group" in p:
+            g = read_group(p["group"])
+            # should warn if g is Null
+            tors = [cc.rt.torsions for cc in monlib.monomers.values()
+                    if cc.group == g]
+        elif "link" in p:
+            tors = [ln.rt.torsions for ln in monlib.links.values()
+                    if fnmatch.fnmatch(ln.id, p["link"])]
+        else:
+            tors = []
+        if not tors:
+            continue
+        logger.writeln(f" rule = {p}")
+        for tt in tors:
+            for t in tt:
+                if fnmatch.fnmatch(t.label, p["tors_name"]):
+                    if "tors_value" in p:
+                        t.value = p["tors_value"]
+                    if "tors_sigma" in p:
+                        t.esd = p["tors_sigma"]
+                    if "tors_period" in p:
+                        t.period = p["tors_period"]
+# update_torsions()
+
+def make_torsion_rules(restr_params):
+    # Defaults
+    include_rules = [{"group": "peptide", "tors_name": "chi*"},
+                     {"link": "*", "tors_name": "omega"},
+                     {"residue": "*", "tors_name": "sp2_sp2*"},
+                     {"link": "*", "tors_name": "sp2_sp2*"},
+                     ]
+    exclude_rules = []
+
+    # Override include/exclude rules
+    for i, name in enumerate(("torsion_include", "torsion_exclude")):
+        rules = (include_rules, exclude_rules)[i]
+        for p in restr_params.get(name, []):
+            r = {}
+            if p["flag"]:
+                for k in "residue", "group", "link":
+                    if k in p:
+                        r[k] = p[k]
+                if r and "tors_name" in p:
+                    r["tors_name"] = p["tors_name"]
+                    rules.append(r)
+            else:
+                rules.clear()
+
+    # How to tell about hydrogen?
+    logger.writeln("Torsion angle rules:")
+    for l, rr in (("include", include_rules), ("exclude", exclude_rules)):
+        logger.writeln(f" {l}:")
+        if not rr:
+            logger.writeln(f"  none")
+        for r in rr:
+            logger.writeln(f"  {r}")
+
+    return include_rules, exclude_rules
+# make_torsion_rules())
+
+def select_restrained_torsions(monlib, include_rules, exclude_rules):
+    ret = {"monomer": {}, "link": {}}
+    
+    # Collect monomer/link related torsions
+    all_tors = {"mon": {}, "link": {}}
+    groups = {}
+    for mon_id in monlib.monomers:
+        mon = monlib.monomers[mon_id]
+        groups.setdefault(mon.group, []).append(mon_id)
+        all_tors["mon"][mon_id] = [x.label for x in mon.rt.torsions]
+    for mod_id in monlib.modifications:
+        mod = monlib.modifications[mod_id]
+        tors = [x.label for x in mod.rt.torsions if chr(x.id1.comp) in ("a", "c")] # don't need delete
+        if not tors: continue
+        gr = read_group(mod.group_id)
+        if mod.comp_id and mod.comp_id in all_tors["mon"]:
+            all_tors["mon"][mod.comp_id].extend(tors)
+        elif not mod.comp_id and gr in groups:
+            for mon_id in groups[gr]:
+                all_tors["mon"][mon_id].extend(tors)
+    for lnk_id in monlib.links:
+        lnk = monlib.links[lnk_id]
+        if lnk.rt.torsions:
+            all_tors["link"][lnk_id] = [x.label for x in lnk.rt.torsions]
+    for k in all_tors:
+        for kk in all_tors[k]:
+            all_tors[k][kk] = set(all_tors[k][kk])
+            
+    # Apply include/exclude rule
+    for mon in all_tors["mon"]:
+        match_f = lambda r: ("tors_name" in r and
+                             ("residue" in r and fnmatch.fnmatch(mon, r["residue"]) or
+                              mon in groups.get(read_group(r.get("group", "")), [])))
+        use_tors = []
+        for r in include_rules:
+            if match_f(r):
+                use_tors.extend(x for x in all_tors["mon"][mon] if fnmatch.fnmatch(x, r["tors_name"]))
+        for r in exclude_rules:
+            if match_f(r):
+                use_tors = [x for x in use_tors if not fnmatch.fnmatch(x, r["tors_name"])]
+        if use_tors:
+            ret["monomer"][mon] = sorted(use_tors)
+    for lnk in all_tors["link"]:
+        match_f = lambda r: ("tors_name" in r and
+                             "link" in r and fnmatch.fnmatch(lnk, r["link"]))
+        use_tors = []
+        for r in include_rules:
+            if match_f(r):
+                use_tors.extend(x for x in all_tors["link"][lnk] if fnmatch.fnmatch(x, r["tors_name"]))
+        for r in exclude_rules:
+            if match_f(r):
+                use_tors = [x for x in use_tors if not fnmatch.fnmatch(x, r["tors_name"])]
+        if use_tors:
+            ret["link"][lnk] = sorted(use_tors)
+
+    return ret
+# select_restrained_torsions()
 
 def prepare_topology(st, monlib, h_change, ignore_unknown_links=False, raise_error=True, check_hydrogen=False,
                      use_cispeps=False, add_metal_restraints=True):

src/refine.cpp
@@ -678,6 +678,8 @@ void add_refine(py::module& m) {
     .def_readwrite("max_vdw_radius", &Geometry::max_vdw_radius)
     // torsion parameters
     .def_readwrite("use_hydr_tors", &Geometry::use_hydr_tors)
+    .def_readwrite("link_tors_names", &Geometry::link_tors_names)
+    .def_readwrite("mon_tors_names", &Geometry::mon_tors_names)
     // ADP restraint parameters
     .def_readwrite("adpr_max_dist", &Geometry::adpr_max_dist)
     .def_readwrite("adpr_d_power", &Geometry::adpr_d_power)

commit 72bab398a296a55832d6889c197f77f8700bf693

refine_geom bug: torsion restraints not used by default introduced in 5bd8d321

servalcat/__init__.py
@@ -6,5 +6,5 @@ This software is released under the
 Mozilla Public License, version 2.0; see LICENSE.
 """
 
-__version__ = '0.4.66'
-__date__ = '2024-04-23'
+__version__ = '0.4.67'
+__date__ = '2024-05-03'

servalcat/refine/refine.py
@@ -56,19 +56,18 @@ class Geom:
             utils.fileio.write_model(self.st, "shaken", pdb=True, cif=True)
         self.use_nucleus = use_nucleus
         self.calc_kwds = {"use_nucleus": self.use_nucleus}
-        if params:
-            exte.read_external_restraints(params.get("exte", []), self.st, self.geom)
-            for k in ("wbond", "wangle", "wtors", "wplane", "wchir", "wvdw", "wncs"):
-                if k in params:
-                    self.calc_kwds[k] = params[k]
-                    logger.writeln("setting geometry weight {}= {}".format(k, params[k]))
-            inc_tors, exc_tors = utils.restraints.make_torsion_rules(params.get("restr", {}))
-            rtors = utils.restraints.select_restrained_torsions(monlib, inc_tors, exc_tors)
-            self.geom.mon_tors_names = rtors["monomer"]
-            self.geom.link_tors_names = rtors["link"]
-            self.group_occ = GroupOccupancy(self.st, params.get("occu"))
-        else:
-            self.group_occ = GroupOccupancy(self.st, None)
+        if params is None:
+            params = {}
+        exte.read_external_restraints(params.get("exte", []), self.st, self.geom)
+        for k in ("wbond", "wangle", "wtors", "wplane", "wchir", "wvdw", "wncs"):
+            if k in params:
+                self.calc_kwds[k] = params[k]
+                logger.writeln("setting geometry weight {}= {}".format(k, params[k]))
+        inc_tors, exc_tors = utils.restraints.make_torsion_rules(params.get("restr", {}))
+        rtors = utils.restraints.select_restrained_torsions(monlib, inc_tors, exc_tors)
+        self.geom.mon_tors_names = rtors["monomer"]
+        self.geom.link_tors_names = rtors["link"]
+        self.group_occ = GroupOccupancy(self.st, params.get("occu"))
         if not self.unrestrained:
             self.geom.load_topo(topo)
             self.check_chemtypes(os.path.join(monlib.path(), "ener_lib.cif"), topo)