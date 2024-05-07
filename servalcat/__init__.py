keitaroyam/servalcat

commit cdfb552b1b480c179aea8d4761c9b4a526b275d0

refine: write stats json every cycle

servalcat/__init__.py
@@ -6,5 +6,5 @@ This software is released under the
 Mozilla Public License, version 2.0; see LICENSE.
 """
 
-__version__ = '0.4.64'
-__date__ = '2024-04-06'
+__version__ = '0.4.65'
+__date__ = '2024-04-19'

servalcat/refine/refine.py
@@ -10,6 +10,7 @@ import os
 import re
 import gemmi
 import numpy
+import json
 import pandas
 import scipy.sparse
 import servalcat # for version
@@ -200,6 +201,22 @@ def show_binstats(df, cycle_number):
                                    float_format="{:.4f}".format)
     logger.writeln(lstr)
 # show_binstats()
+
+def write_stats_json_safe(stats, json_out):
+    tmp = []
+    for s in stats: # stats must be a list of dict
+        tmp.append({})
+        for k in s:
+            if k == "geom":
+                tmp[-1]["geom"] = s["geom"].to_dict()
+            else:
+                tmp[-1][k] = s[k]
+    out_tmp = json_out + ".part"
+    with open(out_tmp, "w") as ofs:
+        json.dump(tmp, ofs, indent=2)
+    os.replace(out_tmp, json_out)
+    logger.writeln(f"Refinement statistics saved: {json_out}")
+# write_stats_json_safe()
 
 class GroupOccupancy:
     # TODO max may not be one. should check multiplicity
@@ -636,7 +653,7 @@ class Refine:
         return ret, shift_scale, f1
 
     def run_cycles(self, ncycles, weight=1, weight_adjust=False, debug=False,
-                   weight_adjust_bond_rmsz_range=(0.5, 1.)):
+                   weight_adjust_bond_rmsz_range=(0.5, 1.), stats_json_out=None):
         self.print_weights()
         stats = [{"Ncyc": 0}]
         self.geom.setup_nonbonded(self.refine_xyz)
@@ -694,6 +711,8 @@ class Refine:
             if self.st_traj is not None:
                 self.st_traj.add_model(self.st[0])
                 self.st_traj[-1].name = str(i+1)
+            if stats_json_out:
+                write_stats_json_safe(stats, stats_json_out)
 
             logger.writeln("")
 
@@ -774,4 +793,5 @@ class Refine:
             raw_remarks.append("REMARK   3")
         self.st.meta.refinement = [ri]
         self.st.raw_remarks = raw_remarks
+
 # class Refine

servalcat/refine/refine_geom.py
@@ -158,13 +158,10 @@ def refine_geom(model_in, monomer_dir, cif_files, h_change, ncycle, output_prefi
         ncslist = False
     geom = Geom(st, topo, monlib, shake_rms=randomize, params=params, ncslist=ncslist)
     refiner = Refine(st, geom)
-    stats = refiner.run_cycles(ncycle)
+    stats = refiner.run_cycles(ncycle,
+                               stats_json_out=output_prefix + "_stats.json")
     refiner.st.name = output_prefix
     utils.fileio.write_model(refiner.st, output_prefix, pdb=True, cif=True)
-    with open(output_prefix + "_stats.json", "w") as ofs:
-        for s in stats: s["geom"] = s["geom"].to_dict()
-        json.dump(stats, ofs, indent=2)
-        logger.writeln("Refinement statistics saved: {}".format(ofs.name))
 # refine_geom()
 
 def main(args):

servalcat/refine/refine_spa.py
@@ -8,7 +8,6 @@ Mozilla Public License, version 2.0; see LICENSE.
 from __future__ import absolute_import, division, print_function, generators
 import gemmi
 import argparse
-import json
 import numpy
 from servalcat.utils import logger
 from servalcat import utils
@@ -235,7 +234,8 @@ def main(args):
 
     stats = refiner.run_cycles(args.ncycle, weight=args.weight,
                                weight_adjust=not args.no_weight_adjust,
-                               weight_adjust_bond_rmsz_range=args.target_bond_rmsz_range)
+                               weight_adjust_bond_rmsz_range=args.target_bond_rmsz_range,
+                               stats_json_out=args.output_prefix + "_stats.json")
     if not args.hklin and not args.no_trim:
         refiner.st.cell = maps[0][0].unit_cell
         refiner.st.setup_cell_images()
@@ -244,12 +244,6 @@ def main(args):
     utils.fileio.write_model(refiner.st, args.output_prefix, pdb=True, cif=True, hout=args.hout)
     if params["write_trajectory"]:
         utils.fileio.write_model(refiner.st_traj, args.output_prefix + "_traj", cif=True)
-    with open(args.output_prefix + "_stats.json", "w") as ofs:
-        for s in stats:
-            if "geom" in s: s["geom"] = s["geom"].to_dict()
-        json.dump(stats, ofs, indent=2)
-        logger.writeln("Refinement statistics saved: {}".format(ofs.name))
-    
     if args.hklin:
         return
         

servalcat/refine/refine_xtal.py
@@ -8,7 +8,6 @@ Mozilla Public License, version 2.0; see LICENSE.
 from __future__ import absolute_import, division, print_function, generators
 import gemmi
 import numpy
-import json
 import os
 import shutil
 import argparse
@@ -212,16 +211,12 @@ def main(args):
                      params=params)
 
     stats = refiner.run_cycles(args.ncycle, weight=args.weight,
-                               weight_adjust=not args.no_weight_adjust)
+                               weight_adjust=not args.no_weight_adjust,
+                               stats_json_out=args.output_prefix + "_stats.json")
     refiner.st.name = args.output_prefix
     utils.fileio.write_model(refiner.st, args.output_prefix, pdb=True, cif=True, hout=args.hout)
     if params["write_trajectory"]:
         utils.fileio.write_model(refiner.st_traj, args.output_prefix + "_traj", cif=True)
-    with open(args.output_prefix + "_stats.json", "w") as ofs:
-        for s in stats:
-            if "geom" in s: s["geom"] = s["geom"].to_dict()
-        json.dump(stats, ofs, indent=2)
-        logger.writeln("Refinement statistics saved: {}".format(ofs.name))
 
     if is_int:
         calculate_maps_int(ll.hkldata, ll.b_aniso, ll.fc_labs, ll.D_labs, centric_and_selections,

commit 12788052628588394a2047bcb0d3bc302dfbf954

refine: write stats json in 0th cycle a bug introduced in cdfb552b

servalcat/__init__.py
@@ -6,5 +6,5 @@ This software is released under the
 Mozilla Public License, version 2.0; see LICENSE.
 """
 
-__version__ = '0.4.67'
-__date__ = '2024-05-03'
+__version__ = '0.4.68'
+__date__ = '2024-05-07'

servalcat/refine/refine.py
@@ -682,6 +682,8 @@ class Refine:
             show_binstats(llstats["bin_stats"], 0)
         if self.adp_mode > 0:
             utils.model.adp_analysis(self.st)
+        if stats_json_out:
+            write_stats_json_safe(stats, stats_json_out)
         occ_refine_flag = self.ll is not None and self.geom.group_occ.groups and self.geom.group_occ.ncycle > 0
 
         for i in range(ncycles):