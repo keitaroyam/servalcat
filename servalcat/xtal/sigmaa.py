888fc0462bc452c5cc78dbecbefc19d4707f2eba
Keitaro Yamashita <keitaroyam@users.noreply.github.com>
2024-05-29 12:12:44 +0900

    sigmaa.process_input: always normalise space group name in the structure Coot does not seem to understand "P-3 1 m" for example

--- servalcat/xtal/sigmaa.py
+++ servalcat/xtal/sigmaa.py
@@ -1092,8 +1092,7 @@ def process_input(hklin, labin, n_bins, free, xyzins, source, d_max=None, d_min=
             logger.writeln("")
 
         for st in sts:
-            if st.find_spacegroup() != sg_use:
-                st.spacegroup_hm = sg_use.xhm()
+            st.spacegroup_hm = sg_use.xhm()
             st.setup_cell_images()
         hkldata.sg = sg_use
 
