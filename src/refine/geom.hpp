e0f6f8281075811a0736cf61524449fcddfbce2c
Keitaro Yamashita <keitaroyam@users.noreply.github.com>
2024-09-17 15:26:48 +0900

    geom: more tuning for von Mises

--- src/refine/geom.hpp
+++ src/refine/geom.hpp
@@ -1606,42 +1606,47 @@ inline double Geometry::Angle::calc(const gemmi::UnitCell& cell, double waskal, 
   const double v1n = std::max(v1.length(), 0.02);
   const double v2n = std::max(v2.length(), 0.02);
   const double v12 = v1.dot(v2);
-  const double cosa = std::min(1., v12 / v1n / v2n);
+  const double cosa = std::max(-1., std::min(1., v12 / v1n / v2n));
   const double sina = std::min(1., std::max(std::sqrt(1 - cosa * cosa), 0.001));
-  const double a = gemmi::deg(std::acos(std::max(-1., std::min(1., cosa))));
+  const double a_rad = std::acos(cosa);
+  const double a = gemmi::deg(a_rad);
   auto closest = find_closest_value(a);
   const double da = a - closest->value;
+  const double a0_rad = gemmi::rad(closest->value);
   const double weight = gemmi::sq(waskal / closest->sigma * (von_mises ? gemmi::deg(1) : 1));
   const double ret = von_mises ? ((1-std::cos(gemmi::rad(da))) * weight) : (da * da * weight * 0.5);
   if (target != nullptr) {
     int ia[3];
     for (int i = 0; i < 3; ++i) ia[i] = target->atom_pos[atoms[i]->serial - 1];
-    gemmi::Vec3 dadx[3]; // scales will be multiplied later
-    dadx[0] = (v2 / (v1n * v2n) - v1 * cosa / (v1n * v1n));
-    dadx[2] = (v1 / (v1n * v2n) - v2 * cosa / (v2n * v2n));
-    dadx[1] = -dadx[0] - dadx[2];
+    gemmi::Vec3 dcosadx[3]; // da/dx cosa
+    dcosadx[0] = (v2 / (v1n * v2n) - v1 * cosa / (v1n * v1n));
+    dcosadx[2] = (v1 / (v1n * v2n) - v2 * cosa / (v2n * v2n));
+    dcosadx[1] = -dcosadx[0] - dcosadx[2];
     if (!same_asu(0))
-      dadx[0] = tr1.mat.transpose().multiply(dadx[0]);
+      dcosadx[0] = tr1.mat.transpose().multiply(dcosadx[0]);
     if (!same_asu(2))
-      dadx[2] = tr2.mat.transpose().multiply(dadx[2]);
+      dcosadx[2] = tr2.mat.transpose().multiply(dcosadx[2]);
     const double deriv_fac = von_mises
       // sin(a-a0) / sina
-      ? (weight * (std::cos(gemmi::rad(closest->value)) - cosa / sina * std::sin(gemmi::rad(closest->value))))
+      ? (weight * (std::cos(a0_rad) - cosa * (std::abs(a_rad - a0_rad) < 1e-4
+                                              ? 1. : (std::sin(a0_rad) / sina))))
       : (weight * da * gemmi::deg(1) / sina);
-    const double secder_fac = von_mises ? (weight / gemmi::sq(sina)) : (weight * gemmi::sq(gemmi::deg(1) / sina));
+    const double secder_fac = von_mises
+      ? (weight / gemmi::sq(sina))
+      : (weight * gemmi::sq(gemmi::deg(1) / sina));
     for(int i = 0; i < 3; ++i)
       if (ia[i] >= 0) {
-        target->incr_vn(ia[i] * 3, deriv_fac, dadx[i]);
-        target->incr_am_diag(ia[i] * 6, secder_fac, dadx[i]);
+        target->incr_vn(ia[i] * 3, deriv_fac, dcosadx[i]);
+        target->incr_am_diag(ia[i] * 6, secder_fac, dcosadx[i]);
       }
     for (int i = 0; i < 2; ++i)
       for (int j = i+1; j < 3; ++j)
         if (ia[i] >= 0 && ia[j] >= 0) {
           auto mp = target->find_restraint(ia[i], ia[j]);
           if (mp.imode == 0) // ia[i] > ia[j]
-            target->incr_am_ndiag(mp.ipos, secder_fac, dadx[i], dadx[j]);
+            target->incr_am_ndiag(mp.ipos, secder_fac, dcosadx[i], dcosadx[j]);
           else
-            target->incr_am_ndiag(mp.ipos, secder_fac, dadx[j], dadx[i]);
+            target->incr_am_ndiag(mp.ipos, secder_fac, dcosadx[j], dcosadx[i]);
         }
     target->target += ret;
   }
