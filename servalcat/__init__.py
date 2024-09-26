eae5808c6f00d658545a46bb776f5d72abf80e76
Keitaro Yamashita <keitaroyam@users.noreply.github.com>
2024-09-26 23:06:31 +0900

    mmcif: _atom_site.auth_comp_id was requested

--- servalcat/__init__.py
+++ servalcat/__init__.py
@@ -6,5 +6,5 @@ This software is released under the
 Mozilla Public License, version 2.0; see LICENSE.
 """
 
-__version__ = '0.4.89'
-__date__ = '2024-09-18'
+__version__ = '0.4.90'
+__date__ = '2024-09-26'
--- servalcat/utils/fileio.py
+++ servalcat/utils/fileio.py
@@ -87,6 +87,7 @@ def write_mmcif(st, cif_out, cif_ref=None):
         groups.cis = True
         groups.conn = True
         groups.software = True
+        groups.auth_all = True
         # FIXME is this all? 
         try:
             doc = read_cif_safe(cif_ref)
@@ -111,7 +112,7 @@ def write_mmcif(st, cif_out, cif_ref=None):
     else:
         st_new.name = st_new.name[:78] # this will become _entry.id
         if "_entry.id" in st_new.info: st_new.info["_entry.id"] = st_new.info["_entry.id"][:78]
-        groups = gemmi.MmcifOutputGroups(True)
+        groups = gemmi.MmcifOutputGroups(True, auth_all=True)
         doc = gemmi.cif.Document()
         block = doc.add_new_block("new")
         st_new.update_mmcif_block(block, groups)
