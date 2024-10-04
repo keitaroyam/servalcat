8acd733e61ee5a201d0fed0ed06487c11e473abd
Keitaro Yamashita <keitaroyam@users.noreply.github.com>
2024-10-04 22:06:34 +0900

    refine_geom --update_dictionary: support model_Cartn_x

--- servalcat/__init__.py
+++ servalcat/__init__.py
@@ -6,5 +6,5 @@ This software is released under the
 Mozilla Public License, version 2.0; see LICENSE.
 """
 
-__version__ = '0.4.92'
-__date__ = '2024-10-01'
+__version__ = '0.4.93'
+__date__ = '2024-10-04'
