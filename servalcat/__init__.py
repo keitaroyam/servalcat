4483dfe1a1c71ca6ca0efb41673f85618f771923
Keitaro Yamashita <keitaroyam@users.noreply.github.com>
2025-06-01 20:40:36 +0900

    bug fix: apply user-specified resolution limits to CC1/2 calculations

--- servalcat/__init__.py
+++ servalcat/__init__.py
@@ -6,5 +6,5 @@ This software is released under the
 Mozilla Public License, version 2.0; see LICENSE.
 """
 
-__version__ = '0.4.109'
-__date__ = '2025-05-22'
+__version__ = '0.4.110'
+__date__ = '2025-06-01'
