f9f953d490acfb3439c05a270ef8c42a9551485f
Keitaro Yamashita <keitaroyam@users.noreply.github.com>
2025-11-23 23:05:10 +0900

    switch to gemmi 0.7.4

--- servalcat/__init__.py
+++ servalcat/__init__.py
@@ -6,5 +6,5 @@ This software is released under the
 Mozilla Public License, version 2.0; see LICENSE.
 """
 
-__version__ = '0.4.125.2'
-__date__ = '2025-11-04'
+__version__ = '0.4.125.3'
+__date__ = '2025-11-23'
